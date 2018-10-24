0adb0af873e0b1c718b3760e2d907d94330afd2e
drkthunder02 <mancuso.christopher@gmail.com>
2018-10-23 23:11:45 -0500

    migration tables

---
+++ database/migrations/2018_10_24_033246_create_oreprices_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrepricesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('oreprices', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('Name');
+            $table->integer('ItemId');
+            $table->decimal('BatchPrice', 20,2);
+            $table->decimal('UnitPrice', 20, 2);
+            $table->decimal('m3Price', 20, 2);
+            $table->string('Time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('oreprices');
+    }
+}
---
+++ database/migrations/2018_10_24_033452_create_config_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateConfigTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('config', function (Blueprint $table) {
+            $table->decimal('RentalTax', 5,2);
+            $table->decimal('AllyRentalTax', 5, 2);
+            $table->decimal('RefineRate', 5, 2);
+            $table->integer('RentalTime');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('config');
+    }
+}
