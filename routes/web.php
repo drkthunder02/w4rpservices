acd8ecf27c56a86fa9c06a1ef90367b35a30d553
drkthunder02 <mancuso.christopher@gmail.com>
2018-10-25 20:20:30 -0500

    modified logout route

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ Auth::routes();
 //Login display pages
 Route::get('/login', 'Auth\LoginController@redirectToProvider');
 Route::get('/callback', 'Auth\LoginController@handleProviderCallback');
+Route::get('/logout', 'Auth\LoginController@logout');
 //Dashboard Controller Display pages
 Route::get('/dashboard', 'DashboardController@index');
 
