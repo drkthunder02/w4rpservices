186e1a4401c7959e21b88beaf3d9a3b8613acc79
drkthunder02 <mancuso.christopher@gmail.com>
2018-10-25 22:32:25 -0500

    stdclass error

---
+++ app/Price.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Price extends Model
+{
+    // Table Name
+    protected $table = 'Prices';
+
+    //Primary Key
+    public $primaryKey = 'id';
+
+    // Timestamps
+    public $timestamps = 'true';
+}
