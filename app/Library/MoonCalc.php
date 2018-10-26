5b27146f440d84aeddbb82ac7e00a091f91aa785
drkthunder02 <mancuso.christopher@gmail.com>
2018-10-25 23:41:17 -0500

    remove db close function as it's no longer in use

--- app/Library/MoonCalc.php
+++ app/Library/MoonCalc.php
@@ -68,7 +68,7 @@ class MoonCalc {
         //Calculate the total to price to be mined in one month
         $totalPriceMined = $firstTotal + $secondTotal + $thirdTotal + $fourthTotal;
         //Calculate the rental price.  Refined rate is already included in the price from rental composition
-        $rentalPrice = $totalPriceMined * ($config->RentalTax / 100.00);
+        $rentalPrice = $totalPriceMined * ($config[0]->RentalTax / 100.00);
         //Format the rental price to the appropriate number
         $rentalPrice = number_format($rentalPrice, "2", ".", ",");
        
