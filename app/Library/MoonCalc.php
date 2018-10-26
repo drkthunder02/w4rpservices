21364dbe55825fa0a9c1b093c74e52248a7b64cc
drkthunder02 <mancuso.christopher@gmail.com>
2018-10-25 23:32:12 -0500

    moon calc

--- app/Library/MoonCalc.php
+++ app/Library/MoonCalc.php
@@ -189,7 +189,7 @@ class MoonCalc {
         $thuliumPrice = DB::select('SELECT Price FROM Prices WHERE ItemId = ? AND Time = ?', [16653, $maxTime]);
         //Get the item compositions
         $items = DB::select('SELECT Name,ItemId FROM ItemComposition');
-        dd($tritanium[0]);
+        dd($tritanium[0]['Price']);
         //Go through each of the items and update the price
         foreach($items as $item) {
             //Get the item composition
