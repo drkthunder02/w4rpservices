4d3b20e83f68a7e71b728a6386d20a2c216e6185
drkthunder02 <mancuso.christopher@gmail.com>
2018-10-25 22:54:03 -0500

    undefined index error

--- app/Library/MoonCalc.php
+++ app/Library/MoonCalc.php
@@ -194,7 +194,7 @@ class MoonCalc {
             //Get the item composition
             $composition = DB::select('SELECT * FROM ItemComposition WHERE ItemId = ?', [$item->ItemId]);
             //Calculate the Batch Price
-            $batchPrice = ( ($composition['Tritanium'] * $tritaniumPrice) +
+            $batchPrice = ( ($composition->Tritanium * $tritaniumPrice) +
                             ($composition['Pyerite'] * $pyeritePrice) +
                             ($composition['Mexallon'] * $mexallonPrice) +
                             ($composition['Isogen'] * $isogenPrice) +
