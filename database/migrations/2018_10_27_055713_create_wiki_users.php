8c5d1e43ea99c7234b7aa3cac19fd33a10ef2029
drkthunder02 <mancuso.christopher@gmail.com>
2018-10-27 02:34:35 -0500

    database

--- database/migrations/2018_10_27_055713_create_wiki_users.php
+++ database/migrations/2018_10_27_055713_create_wiki_users.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatWikiUsers extends Migration
+class CreateWikiUsers extends Migration
 {
     /**
      * Run the migrations.
