b6997c1a90e7a7d35eb9a627e5925582609cc4c3
drkthunder02 <mancuso.christopher@gmail.com>
2018-10-27 22:04:07 -0500

    navbar updated

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -30,23 +30,6 @@
                 </div>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="/moons/display">Display Moons</a>
-            </li>
-            @can('isAdmin')
-            <li class="nav-item">
-                <a class="nav-link" href="/moons/addmoon">Add Moon</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/moons/updatemoon">Update Moon</a>
-            </li>
-            @endcan
-            <li class="nav-item">
-                <a class="nav-link" href="/wiki/register">Wiki Registration</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/wiki/changepassword">Wiki Password Change</a>
-            </li>
-            <li class="nav-item">
                 <a class="nav-link" href="/logout">Logout</a>
             </li>
         </ul>
