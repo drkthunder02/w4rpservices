4cc69dfccb17d981099913488388d100aee9cccb
drkthunder02 <mancuso.christopher@gmail.com>
2018-10-27 21:34:51 -0500

    bootstrap into public css

--- resources/views/layouts/b4.blade.php
+++ resources/views/layouts/b4.blade.php
@@ -9,9 +9,6 @@
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
-
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
     </head>
