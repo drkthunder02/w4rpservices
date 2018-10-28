6d6814577539c47f4f562ca7c22005044576f0d5
drkthunder02 <mancuso.christopher@gmail.com>
2018-10-27 22:21:46 -0500

    navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,3 +1,4 @@
+<div class="container">
 <nav class="navbar navbar-expand-sm navbar-light bg-light">
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -35,3 +36,4 @@
         <!-- </div> -->
     <!-- </div> -->
 </nav>
+</div>
