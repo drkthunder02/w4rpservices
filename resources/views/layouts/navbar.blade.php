227448fe22f763ffcbd7483ad4502338ad20fc13
drkthunder02 <mancuso.christopher@gmail.com>
2018-10-27 22:11:39 -0500

    navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -4,34 +4,34 @@
     </button>
     <div class="container-fluid">
         <div class="navbar-header">
-            <a class="navbar-brand" href="/dashboard">W4RP</a>
             <ul class="navbar-nav">
-                    @can('isAdmin')
-                    <li class="nav-item">
-                        <a class="nav-link" href="/dashboard/finances">Finances</a>
-                    </li>
-                    @endcan
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdoownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Moons</a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropDownMenuLink">
-                            <a class="dropdown-item" href="/moons/display">Display Moons</a>
-                            @can('isAdmin')
-                            <a class="dropdown-item" href="/moons/addmoon">Add Moon</a>
-                            <a class="dropdown-item" href="/moons/updatemoon">Update Moon</a>
-                            @endcan
-                        </div>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdoownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Wiki</a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropDownMenuLink">
-                            <a class="dropdown-item" href="/wiki/register">Registration</a>
-                            <a class="dropdown-item" href="/wiki/changepassword">Change Password</a>
-                        </div>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/logout">Logout</a>
-                    </li>
-                </ul>
+                <a class="navbar-brand" href="/dashboard">W4RP</a>
+                @can('isAdmin')
+                <li class="nav-item">
+                    <a class="nav-link" href="/dashboard/finances">Finances</a>
+                </li>
+                @endcan
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdoownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Moons</a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropDownMenuLink">
+                        <a class="dropdown-item" href="/moons/display">Display Moons</a>
+                        @can('isAdmin')
+                        <a class="dropdown-item" href="/moons/addmoon">Add Moon</a>
+                        <a class="dropdown-item" href="/moons/updatemoon">Update Moon</a>
+                        @endcan
+                    </div>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdoownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Wiki</a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropDownMenuLink">
+                        <a class="dropdown-item" href="/wiki/register">Registration</a>
+                        <a class="dropdown-item" href="/wiki/changepassword">Change Password</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/logout">Logout</a>
+                </li>
+            </ul>
         </div>
         
     </div>
