84c1e9f4cd00731afdf0cef8af3c6f857f32e169
drkthunder02 <mancuso.christopher@gmail.com>
2018-10-27 22:01:05 -0500

    navbar stuff

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,4 +1,7 @@
 <nav class="navbar navbar-expand-sm bg-light">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
     <div class="container-fluid">
         <div class="navbar-header">
             <a class="navbar-brand" href="/dashboard">W4RP</a>
@@ -9,6 +12,23 @@
                 <a class="nav-link" href="/dashboard/finances">Finances</a>
             </li>
             @endcan
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdoownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Moons</a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropDownMenuLink">
+                    <a class="dropdown-item" href="/moons/display">Display Moons</a>
+                    @can('isAdmin')
+                    <a class="dropdown-item" href="/moons/addmoon">Add Moon</a>
+                    <a class="dropdown-item" href="/moons/updatemoon">Update Moon</a>
+                    @endcan
+                </div>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdoownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Wiki</a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropDownMenuLink">
+                    <a class="dropdown-item" href="/wiki/register">Registration</a>
+                    <a class="dropdown-item" href="/wiki/changepassword">Change Password</a>
+                </div>
+            </li>
             <li class="nav-item">
                 <a class="nav-link" href="/moons/display">Display Moons</a>
             </li>
