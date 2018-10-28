f6e8d9b0c75296db31cd432269769bade3adedfc
drkthunder02 <mancuso.christopher@gmail.com>
2018-10-27 22:29:03 -0500

    navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -31,22 +31,34 @@
                 <a class="nav-link" href="/logout">Logout</a>
             </li>
         </ul>
+        <ul class="navbar-nav m1-auto">
+            <li class="nav-item">
+                <a class="nav-link">Right Link 1</a>
+            </li>
+        </ul>
     </nav>
 </div>
 
-<div class="navbar navbar-inverse navbar-fixed-top">
-        <div class="navbar-inner">
-          <div class="container">
-             <a href="#" class="brand">BRAND</a>
-            <ul class="nav pull-right">
-              <li><a href="#">Fixed Link</a></li>
-            </ul>
-            <div class="nav-collapse">
-              <ul class="nav">
-                <li><a href="#">L1</a></li>
-                <li><a href=#">L2</a></li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary rounded">
+<a class="navbar-brand" href="#">Navbar</a>
+<ul class="navbar-nav mr-auto">
+    <li class="nav-item active">
+    <a class="nav-link">Left Link 1</a>
+    </li>
+    <li class="nav-item">
+    <a class="nav-link">Left Link 2</a>
+    </li>
+</ul>
+<ul class="navbar-nav ml-auto">
+    <li class="nav-item">
+    <a class="nav-link">Right Link 1</a>
+    </li>
+    <li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">            Dropdown on Right</a>
+    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+        <a class="dropdown-item" href="#">Action</a>
+        <a class="dropdown-item" href="#">Another action with a lot of text inside of an item</a>
+    </div>
+    </li>
+</ul>
+</nav>
