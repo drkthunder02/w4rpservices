bc9069a85a6717d1fd0d3a0030dc5445df5699f2
drkthunder02 <mancuso.christopher@gmail.com>
2018-10-27 22:22:07 -0500

    navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,4 +1,4 @@
-<div class="container">
+<div class="container-fluid">
 <nav class="navbar navbar-expand-sm navbar-light bg-light">
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
