c0e300687cc574e634e260d0c74eca4e2d62b7fb
drkthunder02 <mancuso.christopher@gmail.com>
2018-10-27 21:28:16 -0500

    bootstrap

--- resources/views/layouts/b4.blade.php
+++ resources/views/layouts/b4.blade.php
@@ -12,11 +12,8 @@
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
 
-        <!-- Bootstrap CSS 
+        <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
-        -->
-        <!-- Styles -->
-        <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet">
     </head>
     <body>
         @include('layouts.navbar')
