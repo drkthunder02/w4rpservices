0885fa43f80f833342b8d8808350f7af0f6a5d9c
drkthunder02 <mancuso.christopher@gmail.com>
2018-10-27 22:12:38 -0500

    navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -32,7 +32,11 @@
                     <a class="nav-link" href="/logout">Logout</a>
                 </li>
             </ul>
-        </div>
-        
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="/logout">Logout</a>
+                </li>
+            </ul>
+        </div> 
     </div>
 </nav>
