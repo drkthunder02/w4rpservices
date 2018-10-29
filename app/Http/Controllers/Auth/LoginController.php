78269cfd9cbd389501d9a8b0df9352ec0a10b779
drkthunder02 <mancuso.christopher@gmail.com>
2018-10-28 23:35:21 -0500

    changes to login controller

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -72,9 +72,7 @@ class LoginController extends Controller
      * Redirect to the dashboard if logging in successfully. 
      */
     public function handleProviderCallback() {
-        if(!Auth::check()) {
-            return redirect()->to('/');
-        }
+        
         $ssoUser = Socialite::driver('eveonline')->user();
         $user = $this->createOrGetUser($ssoUser);
 
