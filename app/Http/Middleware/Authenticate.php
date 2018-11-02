841a13f6c93ea2c67c7d385bbe1b9dd40f551e52
drkthunder02 <mancuso.christopher@gmail.com>
2018-11-02 07:18:08 -0500

    routing

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -14,6 +14,7 @@ class Authenticate extends Middleware
      */
     protected function redirectTo($request)
     {
-        return route('/');
+        //return route('/');
+        return view('welcome');
     }
 }
