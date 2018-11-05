d7e0143e0d048673e4e416b85f95ed3ec92b89b2
drkthunder02 <mancuso.christopher@gmail.com>
2018-11-04 18:15:15 -0600

    create user roles table

--- database/migrations/2018_11_05_001352_create_user_roles_table.php
+++ database/migrations/2018_11_05_001352_create_user_roles_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUserRoles extends Migration
+class CreateUserRolesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,12 @@ class CreateUserRoles extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('user_roles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('role')->default('None');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -23,6 +28,6 @@ class CreateUserRoles extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('user_roles');
     }
 }
