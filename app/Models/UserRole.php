7169a25133d8b37d85dcb51ab33b35a9f1efcf25
drkthunder02 <mancuso.christopher@gmail.com>
2018-11-05 09:27:17 -0600

    relationships for local key and foreign key in UserRole relationship

--- app/Models/UserRole.php
+++ app/Models/UserRole.php
@@ -13,6 +13,6 @@ class UserRole extends Model
     public $timestamps = true;
 
     public function user() {
-        return $this->belongsTo('App\User');
+        return $this->belongsTo('App\User', 'character_id', 'user_id');
     }
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('character_id');
+            $table->string('character_id')->unique();
             $table->string('avatar');
             $table->string('access_token')->nullable();
             $table->string('refresh_token')->nullable();
@@ -25,7 +25,7 @@ class CreateUsersTable extends Migration
             $table->string('owner_hash');
             $table->string('user_type')->default('Guest');
             $table->string('role')->default('Guest');
-            $table->text('scopes')->default('publicData');
+            $table->text('scopes')->nullable();
             $table->string('email')->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
