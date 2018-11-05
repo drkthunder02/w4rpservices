01a04711149b27f9c7708d4f2d965201a8802e52
drkthunder02 <mancuso.christopher@gmail.com>
2018-11-05 09:46:13 -0600

    middleware adjustment

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -44,7 +44,9 @@ class LoginController extends Controller
      */
     public function __construct()
     {
-        //$this->middleware('guest')->except('logout');
+        $this->middleware('guest')->except(['logout', 
+                                            'handleProviderCallback', 
+                                            'redirectToProvider']);
     }
 
     /**
