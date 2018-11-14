4ee83ed3afd5725235911d205ff989cbd10fae0b
drkthunder02 <mancuso.christopher@gmail.com>
2018-11-13 22:20:33 -0600

    body for esi

--- app/Http/Controllers/FleetsController.php
+++ app/Http/Controllers/FleetsController.php
@@ -101,11 +101,9 @@ class FleetsController extends Controller
         $fleet = DB::table('Fleets')->where('fleet', $fleetId)->get();
         //Add a pilot to the fleet
         $error = $newPilot->AddPilot($fleet[0]->character_id, $charId, $fleetId);
-        if($error) {
-            return view('fleets.displayfleets')->with($error)->with('data', null);
-        } else {
-            return redirect('/fleets/display');
-        }
+       
+        $this->displayFleets();
+        //return view('fleets.displayfleets')->with($error)->with('data', null);
     }
 
     public function updateFleet() {
