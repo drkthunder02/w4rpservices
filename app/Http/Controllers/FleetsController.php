7649835675c3f61cae12e5f69171a22acbb8245b
drkthunder02 <mancuso.christopher@gmail.com>
2018-11-13 21:46:35 -0600

    add pilot

--- app/Http/Controllers/FleetsController.php
+++ app/Http/Controllers/FleetsController.php
@@ -97,6 +97,7 @@ class FleetsController extends Controller
     public function addPilot($fleetId, $charId) {
         //Retrieve the fleet data
         $fleet = DB::table('Fleets')->where('fleet', $fleetId)->get();
+        dd($fleet);
         //Add a pilot to the fleet
         $error = $fleet->AddPilot($fleet->character_id, $charId);
 
