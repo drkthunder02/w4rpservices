17326141af0da531f37f3eeec472885151afe6fa
drkthunder02 <mancuso.christopher@gmail.com>
2018-11-13 20:00:17 -0600

    fleets

--- resources/views/fleets/displayfleets.blade.php
+++ resources/views/fleets/displayfleets.blade.php
@@ -4,7 +4,7 @@
     <h1>Work in Progress aka NOTHING WORKS!</h1><br>
     <h2>Fleets</h2>
     @foreach ($fleet as $fl)
-        <a href="{{ route('addpilot', $fl->fleet, [Auth::user()->character_id]) }}">Join {{ $fl->description }}</a>
+        <a href="{{ route('addpilot', $fl['fleet'], [Auth::user()->character_id]) }}">Join {{ $fl['description'] }}</a>
     @endforeach
 </div>
 @endsection
