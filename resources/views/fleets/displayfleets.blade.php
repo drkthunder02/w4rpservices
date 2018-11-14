1b15add39bf34d7fb1acbec3ac97b728562c5989
drkthunder02 <mancuso.christopher@gmail.com>
2018-11-13 20:12:30 -0600

    fleets views

--- resources/views/fleets/displayfleets.blade.php
+++ resources/views/fleets/displayfleets.blade.php
@@ -3,7 +3,7 @@
 <div class="container">
     <h1>Work in Progress aka NOTHING WORKS!</h1><br>
     <h2>Fleets</h2>
-    @for($i = 0; $i < $size; $i++)
+    @for($i = 0; $i < count($data['fc']); $i++)
         <a href="{{ route('addpilot', $data['fc'][$i], [Auth::user()->character_id]) }}">Join {{ $data['description'][$i] }}</a>
     @endfor
 </div>
