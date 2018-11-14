ece581234749366d92ce59b5204c9f6ffae32c72
drkthunder02 <mancuso.christopher@gmail.com>
2018-11-13 20:51:50 -0600

    data

--- app/Http/Controllers/FleetsController.php
+++ app/Http/Controllers/FleetsController.php
@@ -47,7 +47,7 @@ class FleetsController extends Controller
         $size = sizeof($fc);
 
         //Return the view with the array of the fleet
-        return view('fleets.displayfleets')->with('data', $data);
+        return view('fleets.displayfleets')->with($data);
     }
 
     public function registerFleet(Request $request) {
