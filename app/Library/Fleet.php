f0bd25ec08c4decfc50d60826825f6f36c902be2
drkthunder02 <mancuso.christopher@gmail.com>
2018-11-14 20:31:48 -0600

    fleets

--- app/Library/Fleet.php
+++ app/Library/Fleet.php
@@ -39,7 +39,6 @@ class Fleet {
     public function SetFleetUri($fleetUri) {
         //Trim the left side of the fleet number
         $uris  = explode('https://esi.tech.ccp.is/v1/fleets/', $fleetUri);
-        dd($uris);
         //Trim the right side of the fleet number
         $fleetUri = rtrim($uris[1], '/?datasource=tranquility');
         $this->fleet = $fleetUri;
