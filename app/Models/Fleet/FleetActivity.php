971a9efcc607cc9f13be6721dee2fbe0d0a4b41e
drkthunder02 <mancuso.christopher@gmail.com>
2018-12-03 13:56:06 -0600

    created fleetactivity model

--- app/Models/Fleet/FleetActivity.php
+++ app/Models/Fleet/FleetActivity.php
@@ -1,10 +1,36 @@
 <?php
 
-namespace App;
+namespace App\Models\Fleet;
 
 use Illuminate\Database\Eloquent\Model;
 
 class FleetActivity extends Model
 {
-    //
+    // Table Name
+    protected $table = 'fleet_activity_tracking';
+
+    // Primary Key
+    public $primaryKey = 'id';
+
+    // Timestamps
+    public $timestamps = true;
+
+    /**
+     * The attributes that are mass assignable
+     * 
+     * @var array
+     */
+    protected $fillable = [
+        'fleetId',
+        'character_id',
+        'character_name',
+        'corporation_id',
+        'corporation_name',
+        'region',
+        'system',
+        'ship',
+        'ship_type',
+        'created_at',
+        'updated_at',
+    ];
 }
