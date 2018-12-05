6b03f4581a9761a7139faa495d21bf667f0d1a4e
drkthunder02 <mancuso.christopher@gmail.com>
2018-12-05 14:40:48 -0600

    user model

--- app/User.php
+++ app/User.php
@@ -64,7 +64,7 @@ class User extends Authenticatable
     }
 
     public function hasPermission($permission) {
-        $perm = UserPermission::where(['character_id' => $this->character_id, 'permission' => $permission])->get();
+        $perm = DB::table('user_permissions')->where(['character_id' => $this->character_id, 'permission' => $permission])->get();
         dd($perm->permission);
         if(UserPermission::where(['character_id' => $this->character_id, 'permission' => $permission])->get(['permission'])) {
             return true;
