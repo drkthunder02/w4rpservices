a15779ac774bc1e4ba7885e559464351f185cf15
drkthunder02 <mancuso.christopher@gmail.com>
2018-12-05 14:26:17 -0600

    user model

--- app/User.php
+++ app/User.php
@@ -65,7 +65,7 @@ class User extends Authenticatable
 
     public function hasPermission($permission) {
         //Check if the user has a specific permission
-        $perm = DB::table('UserPermission')->where(['character_id' => $this->character_id, 'permission' => $permission])->get('permission');
+        $perm = DB::table('UserPermission')->where(['character_id' => $this->character_id, 'permission' => $permission])->get(['permission']);
         if($perm === $permission) {
             return true;
         } else {
