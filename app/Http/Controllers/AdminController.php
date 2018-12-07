85ef1e9fd97e636383304fe1093f912536055f9d
drkthunder02 <mancuso.christopher@gmail.com>
2018-12-06 23:17:42 -0600

    addPermission

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -34,9 +34,9 @@ class AdminController extends Controller
             $perm->permission = $permission;
             $perm->save();
 
-            return view('admin.dashboard')->with('success', 'User udpated!');
+            return redirect('admin.dashboard')->with('success', 'User udpated!');
         } else {
-            return view('admin.dashboard')->with('error', 'User not updated or already has the permission.');
+            return redirect('admin.dashboard')->with('error', 'User not updated or already has the permission.');
         }
     }
 
