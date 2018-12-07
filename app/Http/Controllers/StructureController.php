3584d7f26c8223afb38a5f0c91b7d1b2fdf9a4dd
drkthunder02 <mancuso.christopher@gmail.com>
2018-12-07 02:51:43 -0600

    structure controller

--- app/Http/Controllers/StructureController.php
+++ app/Http/Controllers/StructureController.php
@@ -59,7 +59,7 @@ class StructureController extends Controller
         /**
          * In this next section we are removing the cost of fuel blocks from one structure
          */
-        dd($hFinances->CalculateFuelBlockCost('market'));
+        dd($citadelCount);
         $monthTaxesMarket = $monthTaxesMarket - ($hFinances->CalculateFuelBlockCost('market') * $citadelCount);
         if($monthTaxesMarket < 0.00) {
             $monthTaxesMarket = 0.00;
