32e0a9f4ac64b86585c48df9eb4849d5e98fefd1
drkthunder02 <mancuso.christopher@gmail.com>
2018-12-06 18:12:19 -0600

    helper for console commands

--- app/Library/FinanceHelper.php
+++ app/Library/FinanceHelper.php
@@ -182,8 +182,8 @@ class FinanceHelper {
             return $character->corporation_id;
         } else {
             //Return the corporation_id if it was found in the database as it is what the calling function is looking for
-            
-            return $found[0]->corporation_id;
+            dd($found);
+            return $found->corporation_id;
         }
     }
 
