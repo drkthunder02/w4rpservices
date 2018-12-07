e6557987ba13051fa535f39e1a42a675e0893d93
drkthunder02 <mancuso.christopher@gmail.com>
2018-12-06 21:39:04 -0600

    stuff

--- app/Http/Middleware/RequireRole.php
+++ app/Http/Middleware/RequireRole.php
@@ -26,7 +26,7 @@ class RequireRole
             'Director' => 3,
             'Admin' => 4,
         ];
-        $check = UserRole::where(['character_id', auth()->user()->character_id])->get(['role']);
+        $check = UserRole::where('character_id', auth()->user()->character_id)->get(['role']);
         dd($check);
         if($ranking[$check[0]->role] === $ranking[$role]) {
             $confirmed = true;
