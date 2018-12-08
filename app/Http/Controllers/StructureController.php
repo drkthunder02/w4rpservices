a8a4755a81dfe10c9a56578e826e4e6044822c68
drkthunder02 <mancuso.christopher@gmail.com>
2018-12-08 00:14:50 -0600

    structure controller

--- app/Http/Controllers/StructureController.php
+++ app/Http/Controllers/StructureController.php
@@ -72,8 +72,8 @@ class StructureController extends Controller
         /**
          * Calculate the final taxes and send to display
          */
-        $tax = CorpStructure::where(['corporation_id' => $corporation, 'structure_type' => 'Citadel'])->avg('tax');
-        $rTax = CorpStructure::where(['corporation_id' => $corporation, 'structure_type' => 'Refinery'])->avg('tax');
+        $tax = CorpStructure::where(['corporation_id' => $corporation, 'structure_type' => 'Citadel'])->avg('tax')->get();
+        $rTax = CorpStructure::where(['corporation_id' => $corporation, 'structure_type' => 'Refinery'])->avg('tax')->get();
         dd($tax);
 
         $monthTaxesMarket = $tempMonthTaxesMarket - $marketFuelCost;
