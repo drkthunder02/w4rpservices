245f92df403c609bf03d87430700561cf6250b18
drkthunder02 <mancuso.christopher@gmail.com>
2018-12-11 18:55:58 -0600

    structure controller

--- resources/views/structures/taxes.blade.php
+++ resources/views/structures/taxes.blade.php
@@ -11,18 +11,25 @@
                 <thead>
                     <th>Month</th>
                     <th>Market Tax</th>
-                    <th>Market Generation</th>
+                    <th>Market Revenue</th>
+                    <th>Refinery Tax</th>
+                    <th>Refinery Revenue</th>
                 </thead>
                 <tbody>
                     <tr>
                         <td>This Month</td>
                         <td>{{ $totalTaxes['thisMonthMarket'] }}</td>
-                        <td>{{ $totalTaxes['thisMoMarketGeneration'] }}</td>
+                        <td>{{ $totalTaxes['thisMonthRevMarket'] }}</td>
+                        <td>{{ $totalTaxes['thisMonthRefinery'] }}</td>
+                        <td>{{ $totalTaxes['thisMonthRevRefinery'] }}</td>
                     </tr>
                     <tr>
                         <td>Last Month</td>
                         <td>{{ $totalTaxes['lastMonthMarket'] }}</td>
                         <td>{{ $totalTaxes['lastMoMarketGeneration'] }}</td>
+                        <td>{{ $totalTaxes['lastMonthRefinery'] }}</td>
+                        <td>{{ $totalTaxes['lastMonthRevRefinery'] }}</td>
+                    </tr>
                     </tr>
                 </tbody>
             </table>
