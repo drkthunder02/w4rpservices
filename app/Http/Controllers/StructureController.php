f4abcbd4c1bcbe01fe0c0672b23a43666f1e6d12
drkthunder02 <mancuso.christopher@gmail.com>
2018-12-14 17:38:48 -0600

    choose corp tax history for admins

--- app/Http/Controllers/StructureController.php
+++ app/Http/Controllers/StructureController.php
@@ -25,7 +25,7 @@ class StructureController extends Controller
 
     public function chooseCorpTaxes() {
         //$corps = DB::table('CorpStructures')->select('corporation_name')->groupBy('corporation_name')->get();
-        $corps = CorpStructure::pluck('corporation_name', 'corporation_id')->groupBy('corporation_name');
+        $corps = CorpStructure::pluck('corporation_name', 'corporation_id');
         dd($corps);
         return view('structures.choosecorporation')->with('corps', $corps);
     }
