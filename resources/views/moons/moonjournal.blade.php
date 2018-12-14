95ae7d65872d545c1610dee482a3dcb9ed319e13
drkthunder02 <mancuso.christopher@gmail.com>
2018-12-13 23:03:34 -0600

    tidying up use declarations and making new journal page for testing

--- resources/views/moons/moonjournal.blade.php
+++ resources/views/moons/moonjournal.blade.php
@@ -2,19 +2,20 @@
 @section('content')
 
 <div class="container col-md-12">
+    {{ var_dump($journal) }}
     <table class="table table-striped">
         <thead>
-            <th>Name</th>
             <th>Date</th>
             <th>Description</th>
+            <th>Reason</th>
             <th>Amount</th>
         </thead>
         <tbody>
             @foreach($journal as $journ)
                 <tr>
-                    <td>{{ $journ->first_party_id }}</td>
                     <td>{{ $journ->date }}</td>
                     <td>{{ $journ->description }}</td>
+                    <td>{{ $journ->reason }}</td>
                     <td>{{ $journ->amount }}</td>
                 </tr>
             @endforeach
