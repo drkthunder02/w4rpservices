fd39743e0b5e95e63ddf58eda1c51e0e2f7cd093
drkthunder02 <mancuso.christopher@gmail.com>
2018-12-26 22:47:46 -0600

    jump bridge statistics

--- resources/views/jumpbridges/all.blade.php
+++ resources/views/jumpbridges/all.blade.php
@@ -1,16 +1,14 @@
 @extends('layouts.b4')
 @section('content')
 <div class="container">
-    <div class="row">
-        <div class="card">
-            <div class="card-header">
-                <h2>Overall Jump Bridge Usage</h2>
-            </div>
-            <div class="card-body">
-                Last 30 Days: {{ $data['30days'] }}<br>
-                Last 60 Days: {{ $data{'60days'} }}<br>
-                Last 90 Days: {{ $data['90days'] }}<br>
-            </div>
+    <div class="card">
+        <div class="card-header">
+            <h2>Overall Jump Bridge Usage</h2>
+        </div>
+        <div class="card-body">
+            Last 30 Days: {{ $data['30days'] }}<br>
+            Last 60 Days: {{ $data{'60days'} }}<br>
+            Last 90 Days: {{ $data['90days'] }}<br>
         </div>
     </div>
 </div>
