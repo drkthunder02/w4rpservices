0510951703939d94982f8fb038e077b94578febd
drkthunder02 <mancuso.christopher@gmail.com>
2018-12-30 01:39:54 -0600

    sendmail

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -21,6 +21,7 @@ class AdminController extends Controller
         //Get the users from the database to allow a selection of users for
         //adding and removing roles and permissions
         $users = User::pluck('name')->sortBy('name');
+        dd($users);
         $permissions = AvailableUserPermission::pluck('permission')->sortBy('permission');
 
         $data = [
