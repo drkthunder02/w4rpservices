f481e2d530c846f0ea41baeef550b83c66365180
drkthunder02 <mancuso.christopher@gmail.com>
2019-01-09 07:07:41 -0600

    permissions system rework

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -9,49 +9,6 @@
     <div class="row">
         <div class="col-md-6 card">
             <div class="card-header">
-                Add Role for User
-            </div>
-            <div class="card-body">
-                {!! Form::open(['action' => 'AdminController@addRole', 'method' => 'POST']) !!}
-                <div class="form-group">
-                    {{ Form::label('user', 'User') }}
-                    {{ Form::select('user', $data['users'], null, ['class'=> 'form-control']) }}
-                    {{ Form::select('role', [
-                        'None' => 'None',
-                        'Guest' => 'Guest',
-                        'User' => 'User',
-                        'Admin' => 'Admin',
-                        ], 'None') }}
-                </div>
-                {{ Form::submit('Submit', ['class' => 'btn btn-primary']) }}
-                {!! Form::close() !!}
-            </div>
-        </div>
-        <div class="col-md-6 card">
-            <div class="card-header">
-                Remove Role from User
-            </div>
-            <div class="card-body">
-                {!! Form::open(['action' => 'AdminController@removeRole', 'method' => 'POST']) !!}
-                <div class="form-group">
-                    {{ Form::label('user', 'User') }}
-                    {{ Form::select('user', $data['users'], null, ['class' => 'form-control']) }}
-                    {{ Form::select('role', [
-                        'None' => 'None',
-                        'Guest' => 'Guest',
-                        'User' => 'User',
-                        'Admin' => 'Admin',
-                        'SuperUser' => 'SuperUser',
-                        ], 'None') }}
-                </div>
-                {{ Form::submit('Submit', ['class' => 'btn btn-primary']) }}
-                {!! Form::close() !!}
-            </div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-md-6 card">
-            <div class="card-header">
                 Add Permission for User
             </div>
             <div class="card-body">
