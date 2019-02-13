fde28d0d999786edfce994dc9a0f8985194450bf
drkthunder02 <mancuso.christopher@gmail.com>
2019-02-13 05:45:53 -0600

    updating moon rentals to utilize carbon library

--- app/Http/Controllers/MoonsAdminController.php
+++ app/Http/Controllers/MoonsAdminController.php
@@ -138,7 +138,9 @@ class MoonsAdminController extends Controller
         foreach($moons as $moon) {
             //Setup formats as needed
             $spm = $moon->System . ' - ' . $moon->Planet . ' - ' . $moon->Moon;
-            $rentalEnd = date('Y-m-d', $moon->RentalEnd);
+            $rentalEnd = new Carbon($moon->RentalEnd);
+            $rentalEnd = $rentalEnd->format('m-d');
+            //$rentalEnd = date('Y-m-d', $moon->RentalEnd);
             $price = $moonCalc->SpatialMoonsOnlyGoo($moon->FirstOre, $moon->FirstQuantity, $moon->SecondOre, $moon->SecondQuantity, 
                                                     $moon->ThirdOre, $moon->ThirdQuantity, $moon->FourthOre, $moon->FourthQuantity);
             //Add the data to the html string to be passed to the view
