9820bb8870f0a1f53febeccfecf4987ad516548c
drkthunder02 <mancuso.christopher@gmail.com>
2019-04-19 22:23:43 -0500

    database table

--- app/Console/Commands/corpJournal.php
+++ app/Console/Commands/corpJournal.php
@@ -64,7 +64,7 @@ class CorpJournalCommand extends Command
             $finance->GetWalletJournal(1, $charId->character_id);
         }
 
-        $finance->GetWalletJournal(3, $charId->94415555);
+        $finance->GetWalletJournal(3, 94415555);
 
         //Mark the job as finished
         $task->SetStopStatus();
