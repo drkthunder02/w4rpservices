f0682702b1bd417c6865d66caf1be11b12e64acc
drkthunder02 <mancuso.christopher@gmail.com>
2019-04-19 22:18:25 -0500

    pi database journals

--- database/migrations/2019_04_19_000000_create_pi_sale_journals_table.php
+++ database/migrations/2019_04_19_000000_create_pi_sale_journals_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePISaleJournal extends Migration
+class CreatePISaleJournals extends Migration
 {
     /**
      * Run the migrations.
