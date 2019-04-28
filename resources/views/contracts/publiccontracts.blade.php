095b6c419fef3210bd0840536bcc0961fa0805cf
drkthunder02 <mancuso.christopher@gmail.com>
2019-04-28 05:03:14 -0500

    contract blade

--- resources/views/contracts/publiccontracts.blade.php
+++ resources/views/contracts/publiccontracts.blade.php
@@ -6,49 +6,8 @@
     </div>
 </div>
 <br>
-{{ dd($data['contracts']) }}
-@if(count($data['contracts']))
-@foreach($data['contracts'] as $contract)
-<div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">
-                        <div class="row">
-                            <div class="col-sm" align="left">
-                                {{ $contract['title'] }}
-                            </div>
-                            <div class="col-sm" align="center">
-                                Type: Public
-                            </div>
-                            <div class="col-sm" align="right">
-                                <a href="/contracts/display/newbid/{{ $contract['contract_id'] }}" class="btn btn-primary" role="button">Bid on Contract</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="card-body">
-                        <div class="container">
-                            End Date: {{ $contract['end_date'] }}
-                        </div>
-                        <span class="border-dark">
-                            <div class="container">
-                                {{ $contract['body'] }}
-                            </div>
-                        </span>
-                        <hr>
-                        <span class="border-dark">
-                            @if(count($data['bids']))
-                                @include('contracts.includes.biddisplay')
-                            @else
-                                No Bids have been entered.
-                            @endif
-                        </span>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-<br>
+@if(count($contracts))
+    @include('contracts.includes.contractsdisplay')
 @else
     @include('contracts.includes.nocontracts')
 @endif
