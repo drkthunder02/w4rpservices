78ce2b21380968fa0ba2efce6d5622c6d1924c49
drkthunder02 <mancuso.christopher@gmail.com>
2019-04-28 20:34:34 -0500

    contracts and update to lookup function

--- app/Http/Controllers/StructureController.php
+++ app/Http/Controllers/StructureController.php
@@ -74,7 +74,7 @@ class StructureController extends Controller
         
         //Get the character's corporation from the lookup table or esi
         $corpId = $helper->LookupCharacter(Auth::user()->character_id);
-        $corpId = $corpId[0]->corporation_id;
+        
         
         //Get the dates we are working with
         $dates = $sHelper->GetTimeFrameInMonths($months);
