f1c22b8f5eccc8fd77c52d4c741064932d2551fa
drkthunder02 <mancuso.christopher@gmail.com>
2019-04-28 22:15:36 -0500

    public contract?

--- resources/views/contracts/publiccontracts.blade.php
+++ resources/views/contracts/publiccontracts.blade.php
@@ -44,6 +44,7 @@
                                     <th>Amount</th>
                                 </thead>
                                 <tbody>
+                                    {{ dd($contracts['bids']) }}
                                 @foreach($contracts['bids'] as $bid)
                                     <tr>
                                         <td>{{ $bid['corporation_name'] }}</td>
