185fe3cca8eb1ee345fd820733d42d8fb445a6ea
drkthunder02 <mancuso.christopher@gmail.com>
2019-05-02 23:34:53 -0500

    ending contracts

--- resources/views/contracts/admin/displayend.blade.php
+++ resources/views/contracts/admin/displayend.blade.php
@@ -41,7 +41,7 @@
                         </thead>
                         <tbody>
                             {!! Form::open(['action' => 'ContractAdminController@endContract', 'method' => 'POST']) !!}
-                            {{ Form::hidden('contract_id', $contract['id']) }}
+                            {{ Form::hidden('contract_id', $contract['contract_id']) }}
                         @foreach($bids as $bid)
                             <tr>
                                 <td>{{ $bid['bid_amount'] }}</td>
