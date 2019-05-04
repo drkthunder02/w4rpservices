dc2001bba3d60d2766f4d6c852cdf2bbf97685bb
drkthunder02 <mancuso.christopher@gmail.com>
2019-05-03 22:55:50 -0500

    process wallet journal job which is a new job to make system faster for cron

--- database/migrations/2019_05_04_00000_create_job_process_wallet_journal.php
+++ database/migrations/2019_05_04_00000_create_job_process_wallet_journal.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class JobProcessWaletJournal extends Migration
+class CreateJobProcessWaletJournal extends Migration
 {
     /**
      * Run the migration
