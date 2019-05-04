6c14e4a2b1aa7120ede363d5437bd0bcb4f4b540
drkthunder02 <mancuso.christopher@gmail.com>
2019-05-03 22:56:24 -0500

    database migration issue

--- database/migrations/2019_05_04_00000_create_job_process_wallet_journal.php
+++ database/migrations/2019_05_04_00000_create_job_process_wallet_journal.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateJobProcessWaletJournal extends Migration
+class CreateJobProcessWalletJournal extends Migration
 {
     /**
      * Run the migration
