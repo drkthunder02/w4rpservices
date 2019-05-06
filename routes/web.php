4cdea604cceb5edad4bf30c3c23ac2e9a1d70f49
drkthunder02 <mancuso.christopher@gmail.com>
2019-05-05 23:01:12 -0500

    organized controllers into folders

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('/', function () {
 
 Route::group(['middleware' => ['auth']], function(){
     //Dashboard Controller Display pages
-    Route::get('/dashboard', 'DashboardController@index');
+    Route::get('/dashboard', 'Dashboard\DashboardController@index');
 
     //Moon Controller display pages
     Route::get('/moons/display', 'Moons\MoonsController@displayMoons');
