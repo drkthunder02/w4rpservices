490714f5c07143cda7175ec763d46bb0663a8907
drkthunder02 <mancuso.christopher@gmail.com>
2019-05-15 07:15:16 -0500

    added Log class use to all jobs

--- app/Jobs/ProcessWallettransactionJob.php
+++ app/Jobs/ProcessWallettransactionJob.php
@@ -8,6 +8,7 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
+use Log;
 
 //App Library
 use App\Library\Finances\Helper\FinanceHelper;
