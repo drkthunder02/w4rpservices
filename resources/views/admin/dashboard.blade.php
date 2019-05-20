2c9ab8fdfca2661871949d821096e62104821215
drkthunder02 <mancuso.christopher@gmail.com>
2019-05-20 01:50:39 -0500

    dashboard css issues

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -17,37 +17,7 @@
 <br>
 <div class="tab-content">
     <div id="user" class="tab-pane active">
-        <div class="container">
-            <div class="row">
-                <table class="table table-striped">
-                    <thead>
-                        <th>Name</th>
-                        <th>Role</th>
-                        <th>Permissions</th>
-                        <th>Action</th>
-                    </thead>
-                    <tbody>
-                        @foreach($userArr as $user)
-                        <tr>
-                            <td>{{ $user['name'] }}</td>
-                            <td>{{ $user['role'] }}</td>
-                            <td>
-                            @foreach($user['permissions'] as $perm)
-                                {{ implode(', ', $perm) }}
-                            @endforeach
-                            </td>
-                            <td>
-                                {!! Form::open(['action' => 'Dashboard\AdminController@removeUser', 'method' => 'POST']) !!}
-                                {{ Form::hidden('user', $user['name']) }}
-                                {{ Form::submit('Remove User', ['class' => 'btn btn-primary']) }}
-                                {!! Form::close() !!}
-                            </td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                <table>
-            </div>
-        </div>
+        placeholder
     </div>
     <div id="permissions" class="tab-pane fade">
         <div class="container">
