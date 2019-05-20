fa3a3a449276e1f01fd21ea238a42646deb8fabb
drkthunder02 <mancuso.christopher@gmail.com>
2019-05-20 00:48:38 -0500

    added new features to the admin panel

--- app/Http/Controllers/Dashboard/AdminController.php
+++ app/Http/Controllers/Dashboard/AdminController.php
@@ -87,15 +87,14 @@ class AdminController extends Controller
          * Example:  userArrs[0]['name'] = Minerva Arbosa
          *           userArrs[0]['permissions'] = ['admin', 'contract.admin', superuser]
          */
-        $users = User::orderBy('name', 'desc')->get();
-        $users = $users->toArray();
+        $users = User::orderBy('name', 'desc')->get()->toArray();
         foreach($users as $user) {
             $permissions = UserPermission::where([
                 'character_id' => $user['character_id'],
             ])->get()->toArray();
 
             $tempUser['name'] =  $user['name'];
-            $tempUser['role'] = $user['role'];
+            $tempUser['role'] = $user['user_type'];
             $tempUser['permissions'] = $permissions;
 
             array_push($userArry, $tempUser);
