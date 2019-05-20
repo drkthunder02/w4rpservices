dae0f5b159f7444c4acfc7d4e71f08de5e6f70fe
drkthunder02 <mancuso.christopher@gmail.com>
2019-05-20 01:44:44 -0500

    old admin dashboard panel

--- resources/views/admin/olddashboard.blade.php
+++ resources/views/admin/olddashboard.blade.php
@@ -141,17 +141,7 @@
         </div>
     </div>
     <div id="wiki" class="tab-pane fade">
-        <div class="container">
-            <div class="row">
-                {!! Form::open(['action' => 'Wiki\WikiController@purgeUsers', 'method' => 'POST']) !!}
-                <div class="form-group">
-                    {{ Form::label('admin', 'This action will log the administrator who peformed the action.') }}
-                    {{ Form::hidden('admin', auth()->user()->character_id) }}
-                </div>
-                {{ Form::submit('Purge Wiki', ['class' => 'btn btn-primary']) }}
-                {!! Form::close() !!}
-            </div>
-        </div>
+        This is a placeholder
     </div>
     <div id="taxes" class="tab-pane fade">
         <div class="container-fluid">
