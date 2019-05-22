d758950189a39f96e8b64130c6e6a773db63b894
drkthunder02 <mancuso.christopher@gmail.com>
2019-05-22 00:29:00 -0500

    datatables admin page

--- resources/views/admin/layouts/b4.blade.php
+++ resources/views/admin/layouts/b4.blade.php
@@ -13,7 +13,6 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
         -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
         <link ref="stylesheet" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
 
         <!-- DataTables Javascript -->
