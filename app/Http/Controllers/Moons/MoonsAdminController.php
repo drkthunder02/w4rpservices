ea8ef913b4d3baab59bd36c73a5c2b4c27f73ed4
drkthunder02 <mancuso.christopher@gmail.com>
2019-05-29 00:10:05 -0500

    moons admin controller

--- app/Http/Controllers/Moons/MoonsAdminController.php
+++ app/Http/Controllers/Moons/MoonsAdminController.php
@@ -92,7 +92,7 @@ class MoonsAdminController extends Controller
             'System' => $request->system,
             'Planet' => $request->planet,
             'Moon' => $request->moon,
-        ])->get();
+        ])->first();
 
         $price = $moonCalc->SpatialMoonsOnlyGoo($moon->FirstOre, $moon->FirstQuantity, $moon->SecondOre, $moon->SecondQuantity, 
                                                     $moon->ThirdOre, $moon->ThirdQuantity, $moon->FourthOre, $moon->FourthQuantity);
