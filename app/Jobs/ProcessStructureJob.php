cb63745b5a9d6452b3fdcb34419be79a975b56a5
drkthunder02 <mancuso.christopher@gmail.com>
2019-05-29 23:48:45 -0500

    more logging info to try

--- app/Jobs/ProcessStructureJob.php
+++ app/Jobs/ProcessStructureJob.php
@@ -230,6 +230,8 @@ class ProcessStructureJob implements ShouldQueue
         } catch(RequestFailedException $e) {
             Log::warning("Failed to get structure information for structure with id " . $structureId);
             Log::warning(var_dump($info));
+            Log::warning($e->getCode());
+            Log::warning($e->getMessage());
             Log::warning($e->getEsiResponse());
             $info = null;
         }
