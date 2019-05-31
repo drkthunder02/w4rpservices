25c7ded89804cdf292282bd0acaed053d2ca9232
drkthunder02 <mancuso.christopher@gmail.com>
2019-05-31 00:08:37 -0500

    testing

--- app/Library/Structures/StructureHelper.php
+++ app/Library/Structures/StructureHelper.php
@@ -52,7 +52,7 @@ class StructureHelper {
         //Setup the esi authentication container
         $config = config('esi');
         //Get the refresh token from the database
-        $token = EsiToken::where(['character_id' => $charId])->get(['refresh_token']);
+        $token = EsiToken::where(['character_id' => $this->charId])->get(['refresh_token']);
         $authentication = new EsiAuthentication([
             'client_id' => $config['client_id'],
             'secret' => $config['secret'],
