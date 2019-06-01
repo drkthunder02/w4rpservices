82f91861f4d26f7aad3562f0781807c4f49e90e4
drkthunder02 <mancuso.christopher@gmail.com>
2019-05-31 21:37:35 -0500

    moon mailer

--- app/Console/Commands/moonmailer.php
+++ app/Console/Commands/moonmailer.php
@@ -82,7 +82,6 @@ class MoonMailerCommand extends Command
             
             //Totalize the cost of the moons
             $cost = $this->TotalizeMoonCost($rentals);
-            dd($cost);
 
             //Get the list of moons in a list format
             $listItems = $this->GetMoonList($rentals);
@@ -184,7 +183,7 @@ class MoonMailerCommand extends Command
             //Get the updated price for the moon
             $price = $moonCalc->SpatialMoonsOnlyGoo($moon->FirstOre, $moon->FirstQuantity, $moon->SecondOre, $moon->SecondQuantity, 
                                                     $moon->ThirdOre, $moon->ThirdQuantity, $moon->FourthOre, $moon->FourthQuantity);
-
+            dd($price);
             //Check the type and figure out which price to add in
             if($rental->Type == 'alliance') {
                 $totalCost += $price['alliance'];
