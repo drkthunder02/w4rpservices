c675bed506ca368031058c39f0d86f0bddcd85ff
drkthunder02 <mancuso.christopher@gmail.com>
2019-06-25 01:36:10 -0500

    created modify user functionality on admin dashboard

--- resources/views/admin/user/modify.blade.php
+++ resources/views/admin/user/modify.blade.php
@@ -1,39 +1,51 @@
-@extends('layouts.b4')
+@extends('admin.layouts.b4')
 @section('content')
 <br>
+<div class="container">
+    <div class="row justify-content-center">
+        <h2>Modify User Section</h2>
+    </div>
+</div>
+<div class="container">
+    <div class="row justify-content-center">
+        <h3>User: {{ $user->name }}</h3>
+    </div>
+</div>
 <div class="container-fluid">
     <div class="row">
-        <div class="card">
-            <div class="card-header">
-                <h2>Modify User</h2>
-                <h3>User: {{ $user->name }}</h3>
-            </div>
-            <div class="card-body">
-                {{ Form::open(['action' => 'Dashboard\AdminController@addPermission', 'method' => 'POST']) }}
-                <div class="form-group">
-                {{ Form::hidden('user', $user->name) }}
-                {{ Form::select('permission', [
-                    'structure.operator' => 'structure.operator',
-                    'logistics.minion' => 'logistics.minion',
-                    'admin.finance' => 'admin.finance',
-                    'contract.admin' => 'contract.admin',
-                    'contract.canbid' => 'contract.canbid',
-                ], 'None') }}
-                {{ Form::hidden('type', 'addPermission') }}
+        <div class="col">
+            <div class="card">
+                <div class="card-header">
+                    <h3>Add Permission</h3>
                 </div>
-                <div class="form-group col-md-2">
-                    {{ Form::submit('Add Permission', ['class' => 'btn btn-primary']) }}
-                    {!! Form::close() !!}
+                <div class="card-body">
+                    {{ Form::open(['action' => 'Dashboard\AdminController@addPermission', 'method' => 'POST']) }}
+                    <div class="form-group">
+                    {{ Form::hidden('user', $user->name) }}
+                    {{ Form::select('permission', [
+                        'structure.operator' => 'structure.operator',
+                        'logistics.minion' => 'logistics.minion',
+                        'admin.finance' => 'admin.finance',
+                        'contract.admin' => 'contract.admin',
+                        'contract.canbid' => 'contract.canbid',
+                    ], 'None') }}
+                    {{ Form::hidden('type', 'addPermission') }}
+                    </div>
+                    <div class="form-group col-md-2">
+                        {{ Form::submit('Add Permission', ['class' => 'btn btn-primary']) }}
+                        {!! Form::close() !!}
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="card">
-            <div class="card-header">
-                <h2>Modify Role</h2>
-                <h3>User: {{ $user->name }}</h3>
-            </div>
-            <div class="card-body">
+        <div class="col">
+            <div class="card">
+                <div class="card-header">
+                    <h2>Modify Role</h2>
+                </div>
+                <div class="card-body">
 
+                </div>
             </div>
         </div>
     </div>
