701c633cc5ff130e7740f7ef9bf3d55ba664b8af
drkthunder02 <mancuso.christopher@gmail.com>
2019-06-30 02:01:34 -0500

    srp ships

--- app/Http/Controllers/SRP/SRPController.php
+++ app/Http/Controllers/SRP/SRPController.php
@@ -34,11 +34,8 @@ class SRPController extends Controller
             $shipTypes[$type->code] = $type->description;
         }
 
-        dd($shipTypes);
-
         foreach($fleetTypesTemp as $type) {
-            $temp[$type->code] = $type->description;
-            array_push($fleetTypes, $temp);
+            $fleetTypes[$type->code] = $type->description;
         }
 
         return view('srp.srpform')->with('fleetTypes', $fleetTypes)
