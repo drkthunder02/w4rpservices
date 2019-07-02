5a6c7da5490b170102cddef101848f9af12dbf1b
drkthunder02 <mancuso.christopher@gmail.com>
2019-07-01 22:02:14 -0500

    srp helper

--- resources/views/srp/admin/statistics.blade.php
+++ resources/views/srp/admin/statistics.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.b4')
 @section('content')
 
-@barchart('FC Losses', 'fc_loss_div')
+@barchart('ISK, 'fc_loss_div')
 <div id="poll_div"></div>
-{{ $lava->render('BarChart', 'FC Losses', 'poll_div') }}
+{{ $lava->render('BarChart', 'ISK', 'poll_div') }}
 
 @endsection
