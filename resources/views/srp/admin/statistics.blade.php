20ec956b77bc595d68c014f3ed6c643ce4f9426f
drkthunder02 <mancuso.christopher@gmail.com>
2019-07-01 21:56:13 -0500

    srp helper

--- resources/views/srp/admin/statistics.blade.php
+++ resources/views/srp/admin/statistics.blade.php
@@ -3,6 +3,6 @@
 
 @barchart('FC Losses', 'fc_loss_div')
 <div id="poll_div"></div>
-{{ Lava::render('BarChart', 'Food Poll', 'poll_div') }}
+{{ Lava::render('BarChart', 'FC Losses', 'poll_div') }}
 
 @endsection
