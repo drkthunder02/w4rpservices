f0543da790a3ce94dc70f998b0b1f607ac687fcf
drkthunder02 <mancuso.christopher@gmail.com>
2019-07-04 01:17:03 -0500

    updated some lava code

--- resources/views/srp/admin/statistics.blade.php
+++ resources/views/srp/admin/statistics.blade.php
@@ -3,16 +3,16 @@
 
 <div class="container">
     <div id="chart-div"></div>
-    {!! $lava->render('PieChart', 'SRP Stats', 'chart-div') !!}
+    {!! Lava::render('PieChart', 'SRP Stats', 'chart-div') !!}
 </div>
 
 <div class="container">
     <div id="under-review-div"></div>
-    {!! $lava->render('GaugeChart', 'SRP', 'under-review-div') !!}
+    {!! Lava::render('GaugeChart', 'SRP', 'under-review-div') !!}
 </div>
 
 <div class="container">
     <div id="fc-losses-div"></div>
-    {!! $lava->render('BarChart', 'FCs', 'fc-losses-div') !!}
+    {!! Lava::render('BarChart', 'FCs', 'fc-losses-div') !!}
 </div>
 @endsection
