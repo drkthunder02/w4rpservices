eb2f2f86f010a0ffe823bf647965ef2ce4fc229f
drkthunder02 <mancuso.christopher@gmail.com>
2019-07-04 01:13:39 -0500

    updated composer and deleted other chart libraries other than lavacharts

--- config/app.php
+++ config/app.php
@@ -153,8 +153,6 @@ return [
         Collective\Html\HtmlServiceProvider::class,
         //nullx27\Socialite\EveOnline\Providers\EveOnlineServiceProvider::class,
         Laravel\Socialite\SocialiteServiceProvider::class,
-        Fx3costa\LaravelChartJs\Providers\ChartjsServiceProvider::class,
-        ConsoleTVs\Charts\ChartsServiceProvider::class,
         Khill\Lavacharts\Laravel\LavachartsServiceProvider::class,
 
 
@@ -219,7 +217,7 @@ return [
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
         'Socialite' => Laravel\Socialite\Facades\Socialite::class,
-        //'Lava' => Khill\Lavacharts\Laravel\LavachartsFacade::class,
+        'Lava' => Khill\Lavacharts\Laravel\LavachartsFacade::class,
     ],
 
 ];
