004eb146bfd2e1c58d5923c168bfc4ad5c9a18cb
drkthunder02 <mancuso.christopher@gmail.com>
2019-08-06 12:47:07 -0500

    testing

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -22,46 +22,44 @@
 <br>
 <div class="container col-md-12">
     <div class="row justify-content-center">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header">
-                    <div class="row">
-                        <div class="col">
-                            Open SRP Requests<br>
-                            # Open: {{ $openCount }}<br>
-                        </div>
-                        <div class="col">
-                            <div id="under-review-div"></div>
-                            {!! $lava->render('GaugeChart', 'SRP', 'under-review-div') !!}
-                        </div>
+        <div class="card">
+            <div class="card-header">
+                <div class="row">
+                    <div class="col">
+                        Open SRP Requests<br>
+                        # Open: {{ $openCount }}<br>
+                    </div>
+                    <div class="col">
+                        <div id="under-review-div"></div>
+                        {!! $lava->render('GaugeChart', 'SRP', 'under-review-div') !!}
                     </div>
                 </div>
-                <div class="card-body">
-                    @if($openCount > 0)
-                    <table class="table table-striped">
-                        <thead>
-                            <th>Character</th>
-                            <th>Fleet Commander</th>
-                            <th>Ship Type</th>
-                            <th>Loss Value</th>
-                            <th>Status</th>
-                        </thead>
-                        <tbody>
-                            @foreach($open as $o)
-                            <tr>
-                                <td>{{ $o['character_name'] }}</td>
-                                <td>{{ $o['fleet_commander_name'] }}</td>
-                                <td>{{ $o['ship_type'] }}</td>
-                                <td>{{ $o['loss_value'] }}</td>
-                                <td>{{ $o['approved'] }}</td>
-                            </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                    @else
-                    No Open SRP Requests
-                    @endif
-                </div>
+            </div>
+            <div class="card-body">
+                @if($openCount > 0)
+                <table class="table table-striped">
+                    <thead>
+                        <th>Character</th>
+                        <th>Fleet Commander</th>
+                        <th>Ship Type</th>
+                        <th>Loss Value</th>
+                        <th>Status</th>
+                    </thead>
+                    <tbody>
+                        @foreach($open as $o)
+                        <tr>
+                            <td>{{ $o['character_name'] }}</td>
+                            <td>{{ $o['fleet_commander_name'] }}</td>
+                            <td>{{ $o['ship_type'] }}</td>
+                            <td>{{ $o['loss_value'] }}</td>
+                            <td>{{ $o['approved'] }}</td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+                @else
+                No Open SRP Requests
+                @endif
             </div>
         </div>
     </div>
@@ -69,40 +67,38 @@
 <br>
 <div class="container col-md-12">
     <div class="row justify-content-center">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header">
-                    Denied SRP Requests<br>
-                    # Denied: {{ $deniedCount }}
-                </div>
-                <div class="card-body">
-                    @if($deniedCount > 0)
-                    <table class="table table-striped">
-                        <thead>
-                            <th>Character</th>
-                            <th>Fleet Commander</th>
-                            <th>Ship Type</th>
-                            <th>Loss Value</th>
-                            <th>Status</th>
-                            <th>Notes</th>
-                        </thead>
-                        <tbody>
-                            @foreach($denied as $d)
-                            <tr>
-                                <td>{{ $d['character_name'] }}</th>
-                                <td>{{ $d['fleet_commander_name'] }}</td>
-                                <td>{{ $d['ship_type'] }}</td>
-                                <td>{{ $d['loss_value'] }}</td>
-                                <td>{{ $d['approved'] }}</td>
-                                <td>{{ $d['notes'] }}</td>
-                            </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                    @else
-                    No Denied SRP Requests
-                    @endif
-                </div>
+        <div class="card">
+            <div class="card-header">
+                Denied SRP Requests<br>
+                # Denied: {{ $deniedCount }}
+            </div>
+            <div class="card-body">
+                @if($deniedCount > 0)
+                <table class="table table-striped">
+                    <thead>
+                        <th>Character</th>
+                        <th>Fleet Commander</th>
+                        <th>Ship Type</th>
+                        <th>Loss Value</th>
+                        <th>Status</th>
+                        <th>Notes</th>
+                    </thead>
+                    <tbody>
+                        @foreach($denied as $d)
+                        <tr>
+                            <td>{{ $d['character_name'] }}</th>
+                            <td>{{ $d['fleet_commander_name'] }}</td>
+                            <td>{{ $d['ship_type'] }}</td>
+                            <td>{{ $d['loss_value'] }}</td>
+                            <td>{{ $d['approved'] }}</td>
+                            <td>{{ $d['notes'] }}</td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+                @else
+                No Denied SRP Requests
+                @endif
             </div>
         </div>
     </div>
@@ -110,38 +106,36 @@
 <br>
 <div class="container col-md-12">
     <div class="row justify-content-center">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header">
-                    Approved SRP Requests<br>
-                    # Approved: {{ $approvedCount }}
-                </div>
-                <div class="card-body">
-                    @if($approvedCount > 0)
-                    <table class="table table-striped">
-                        <thead>
-                            <th>Character</th>
-                            <th>Fleet Commander</th>
-                            <th>Ship Type</th>
-                            <th>Loss Value</th>
-                            <th>Status</th>
-                        </thead>
-                        <tbody>
-                            @foreach($approved as $a)
-                            <tr>
-                                <td>{{ $a['character_name'] }}</td>
-                                <td>{{ $a['fleet_commander_name'] }}</td>
-                                <td>{{ $a['ship_type'] }}</td>
-                                <td>{{ $a['loss_value'] }}</td>
-                                <td>{{ $a['approved'] }}</td>
-                            </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                    @else
-                    No Approved SRP Requests
-                    @endif
-                </div>
+        <div class="card">
+            <div class="card-header">
+                Approved SRP Requests<br>
+                # Approved: {{ $approvedCount }}
+            </div>
+            <div class="card-body">
+                @if($approvedCount > 0)
+                <table class="table table-striped">
+                    <thead>
+                        <th>Character</th>
+                        <th>Fleet Commander</th>
+                        <th>Ship Type</th>
+                        <th>Loss Value</th>
+                        <th>Status</th>
+                    </thead>
+                    <tbody>
+                        @foreach($approved as $a)
+                        <tr>
+                            <td>{{ $a['character_name'] }}</td>
+                            <td>{{ $a['fleet_commander_name'] }}</td>
+                            <td>{{ $a['ship_type'] }}</td>
+                            <td>{{ $a['loss_value'] }}</td>
+                            <td>{{ $a['approved'] }}</td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+                @else
+                No Approved SRP Requests
+                @endif
             </div>
         </div>
     </div>
