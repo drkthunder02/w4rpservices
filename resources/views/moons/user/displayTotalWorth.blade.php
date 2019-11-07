e3e9d39858e5d3e14327c85fa832ae16ddb7c6d2
drkthunder02 <mancuso.christopher@gmail.com>
2019-11-06 23:12:44 -0600

    moon calc display total worth

--- resources/views/moons/user/displayTotalWorth.blade.php
+++ resources/views/moons/user/displayTotalWorth.blade.php
@@ -3,8 +3,28 @@
 <div class="container">
     <h2>Total Worth of the Moon / Month</h2>
     <div class="jumbotron">
-        Total Moon Goo + Ore Worth: <?php echo $totalWorth; ?> ISK<br>
-        Total Moon Goo Worth: <?php echo $totalGoo; ?> ISK<br>
+        Total Moon Goo + Ore Worth: {{ $totalWorth }} ISK<br>
+        Total Moon Goo Worth: {{ $totalGoo }} ISK<br>
+    </div>
+</div>
+<br>
+<div class="container">
+    <h2>Moon Composition</h2>
+    <div class="jumbotron">
+        <table class="table table-striped table-bordered">
+            <thead>
+                <th>Mineral</th>
+                <th>Quantity</th>
+            </thead>
+            <tbody>
+                @foreach($composition as $key => $value)
+                    <tr>
+                        <td>{{ $key }}</td>
+                        <td>{{ $value }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
 </div>
 @endsection
