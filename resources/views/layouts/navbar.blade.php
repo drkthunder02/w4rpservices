8cacabf1c9aa04a66adb6ded1b498bbc17c3e9c6
drkthunder02 <mancuso.christopher@gmail.com>
2019-11-06 23:16:44 -0600

    rearranged navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -5,6 +5,7 @@
         </button>
         <a class="navbar-brand" href="/dashboard">W4RP</a>
         <ul class="navbar-nav mr-auto">
+            @if(auth()->user()->hasRole('User') || auth()->user()->hasRole('Renter') || auth()->user()->hasRole('Admin'))
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdoownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Moons</a>
                 <div class="dropdown-menu" aria-labelledby="navbarDropDownMenuLink">
@@ -20,37 +21,6 @@
                     @endif
                     @if(auth()->user()->hasPermission('logistics.manager'))
                     <a class="dropdown-item" href="/moons/logistics/display">Moons for Logistics</a>
-                    @endif
-                </div>
-            </li>
-            @if(auth()->user()->hasRole('User') || auth()->user()->hasRole('Admin') || auth()->user()->hasRole('Renter'))
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Wiki</a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropDownMenuLink">
-                    <a class="dropdown-item" href="https://www.w4rp.space">Wiki</a>
-                    <a class="dropdown-item" href="/wiki/register">Registration</a>
-                    <a class="dropdown-item" href="/wiki/changepassword">Change Password</a>
-                </div>
-            </li>
-            @endif
-            @if(auth()->user()->hasRole('User') || auth()->user()->hasRole('Admin'))
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Others</a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropDownMenuLink">
-                    <a class="dropdown-item" href="https://buyback.w4rp.space">Buyback Program</a>
-                </div>
-            </li>
-            @endif
-            @if(auth()->user()->hasRole('User') || auth()->user()->hasRole('Admin'))
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropDownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Contracts</a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropDownMenuLink">
-                    <a class="dropdown-item" href="/contracts/display/all">Display All Contracts</a>
-                    <a class="dropdown-item" href="/contracts/display/public">Display Public Contracts</a>
-                    <a class="dropdown-item" href="/contracts/display/private">Display Private Contracts</a>
-                    @if(auth()->user()->hasPermission('contract.admin'))
-                    <a class="dropdown-item" href="/contracts/admin/display">Admin Dashboard</a>
-                    <a class="dropdown-item" href="/contracts/admin/new">New Contract</a>
                     @endif
                 </div>
             </li>
@@ -71,6 +41,20 @@
             @endif
             @if(auth()->user()->hasRole('User') || auth()->user()->hasRole('Admin'))
             <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropDownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Contracts</a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropDownMenuLink">
+                    <a class="dropdown-item" href="/contracts/display/all">Display All Contracts</a>
+                    <a class="dropdown-item" href="/contracts/display/public">Display Public Contracts</a>
+                    <a class="dropdown-item" href="/contracts/display/private">Display Private Contracts</a>
+                    @if(auth()->user()->hasPermission('contract.admin'))
+                    <a class="dropdown-item" href="/contracts/admin/display">Admin Dashboard</a>
+                    <a class="dropdown-item" href="/contracts/admin/new">New Contract</a>
+                    @endif
+                </div>
+            </li>
+            @endif
+            @if(auth()->user()->hasRole('User') || auth()->user()->hasRole('Admin'))
+            <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropDownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Structures</a>
                 <div class="dropdown-menu" aria-labelledby="navbarDropDownMenuLink">
                     @if(auth()->user()->hasPermission('fc.team'))
@@ -82,6 +66,24 @@
             @endif
             @if(auth()->user()->hasRole('User') || auth()->user()->hasRole('Admin'))
                 <a class="nav-link" href="/logistics/fuel/structures">Jump Gate Fuel</a>
+            @endif
+            @if(auth()->user()->hasRole('User') || auth()->user()->hasRole('Admin') || auth()->user()->hasRole('Renter'))
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Wiki</a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropDownMenuLink">
+                    <a class="dropdown-item" href="https://www.w4rp.space">Wiki</a>
+                    <a class="dropdown-item" href="/wiki/register">Registration</a>
+                    <a class="dropdown-item" href="/wiki/changepassword">Change Password</a>
+                </div>
+            </li>
+            @endif
+            @if(auth()->user()->hasRole('User') || auth()->user()->hasRole('Admin'))
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Others</a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropDownMenuLink">
+                    <a class="dropdown-item" href="https://buyback.w4rp.space">Buyback Program</a>
+                </div>
+            </li>
             @endif
         </ul>
         <ul class="navbar-nav m1-auto">
