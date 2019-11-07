8d0736e54c572abb9b836a4c0341bdb0096e274e
drkthunder02 <mancuso.christopher@gmail.com>
2019-11-07 00:19:19 -0600

    testing

--- resources/views/moons/user/displayTotalWorth.blade.php
+++ resources/views/moons/user/displayTotalWorth.blade.php
@@ -11,6 +11,9 @@
 <div class="container">
     <h2>Moon Composition</h2>
     <div class="jumbotron">
+        <div class="container">
+            <h3>Reprocessing Percentage set at: {{ $reprocessing }}</h3>
+        </div>
         <table class="table table-striped table-bordered">
             <thead>
                 <th>Mineral</th>
