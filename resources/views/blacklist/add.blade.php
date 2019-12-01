0ab18c9ebe776dd2a52d7ba3c743ee09e7f609d3
drkthunder02 <mancuso.christopher@gmail.com>
2019-12-01 03:53:49 -0600

    message

--- resources/views/blacklist/add.blade.php
+++ resources/views/blacklist/add.blade.php
@@ -2,20 +2,28 @@
 @section('content')
 <br>
 <div class="container">
-    {!! Form::open(['action' => 'Blacklist\BlacklistController@AddToBlacklist', 'method' => 'POST']) !!}
-    <div class="form-group">
-        {{ Form::label('name', 'Character Name') }}
-        {{ Form::text('name', '', ['class' => 'form-control', 'placeholder' => 'CCP Antiquarian']) }}
+    <div class="card">
+        <div class="card-header">
+            <h2>Add User to the Blacklist</h2>
+        </div>
+        <div class="card-body">
+            {!! Form::open(['action' => 'Blacklist\BlacklistController@AddToBlacklist', 'method' => 'POST']) !!}
+            <div class="form-group">
+                {{ Form::label('name', 'Character Name') }}
+                {{ Form::text('name', '', ['class' => 'form-control', 'placeholder' => 'CCP Antiquarian']) }}
+            </div>
+            <div class="form-group">
+                {{ Form::label('reason', 'Reason') }}
+                {{ Form::textarea('reason', '', ['class' => 'form-control', 'placeholder' => 'Just another antiquated dev.']) }}
+            </div>
+            <div class="form-group">
+                {{ Form::label('alts', 'Known Alts') }}
+                {{ Form::textarea('alts', '', ['class' => 'form-control']) }}
+            </div>
+            {{ Form::submit('Submit', ['class' => 'btn btn-primary']) }}
+            {!! Form::close() !!}
+        </div>
     </div>
-    <div class="form-group">
-        {{ Form::label('reason', 'Reason') }}
-        {{ Form::textarea('reason', '', ['class' => 'form-control', 'placeholder' => 'Just another antiquated dev.']) }}
-    </div>
-    <div class="form-group">
-        {{ Form::label('alts', 'Known Alts') }}
-        {{ Form::textarea('alts', '', ['class' => 'form-control']) }}
-    </div>
-    {{ Form::submit('Submit', ['class' => 'btn btn-primary']) }}
-    {!! Form::close() !!}
+    
 </div>
 @endsection
