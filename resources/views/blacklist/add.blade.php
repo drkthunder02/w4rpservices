f33d1b540700f82a0abecc977e108a8b6e95c0e7
drkthunder02 <mancuso.christopher@gmail.com>
2019-12-01 02:35:56 -0600

    user redirect

--- resources/views/blacklist/add.blade.php
+++ resources/views/blacklist/add.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.b4')
 @section('content')
 <div class="container">
-    {!! Form::open(['action' => 'Corps\BlacklistController@AddToBlacklist', 'method' => 'POST']) !!}
+    {!! Form::open(['action' => 'Blacklist\BlacklistController@AddToBlacklist', 'method' => 'POST']) !!}
     <div class="form-group">
         {{ Form::label('name', 'Character Name') }}
         {{ Form::text('name', '', ['class' => 'form-control', 'placeholder' => 'CCP Antiquarian']) }}
