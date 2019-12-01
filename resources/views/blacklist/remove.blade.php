d16e0282760f230ed03998f3e1ffe0ac46e6a49e
drkthunder02 <mancuso.christopher@gmail.com>
2019-12-01 02:55:48 -0600

    blacklist

--- resources/views/blacklist/remove.blade.php
+++ resources/views/blacklist/remove.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.b4')
 @section('content')
 <div class="container">
-    {!! Form::open(['action' => 'Corps\BlacklistController@RemoveFromBlacklist', 'method' => 'POST']) !!}
+    {!! Form::open(['action' => 'Blacklist\BlacklistController@RemoveFromBlacklist', 'method' => 'POST']) !!}
     <div class="form-group">
         {{ Form::label('name', 'Character Name') }}
         {{ Form::text('name', '', ['class' => 'form-control', 'placeholder' => 'CCP Antiquarian']) }}
