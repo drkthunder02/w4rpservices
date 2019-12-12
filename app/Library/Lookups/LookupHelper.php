15139add2067258c283ef3fd13b0646fcfd4966c
drkthunder02 <mancuso.christopher@gmail.com>
2019-12-12 00:10:54 -0600

    damn lookup helper

--- app/Library/Lookups/LookupHelper.php
+++ app/Library/Lookups/LookupHelper.php
@@ -151,8 +151,7 @@ class LookupHelper {
         $char = $this->LookupCharacter($charId, null);
 
         //if the character was not found in the database, then get the information and store it in our database for later
-        if($char === null) {
-
+        if(!isset($char->id)) {
             try {
                 $response = $this->esi->invoke('get', '/characters/{character_id}/', [
                     'character_id' => $charId,
