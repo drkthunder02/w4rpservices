3484ebf04e06375b73248f257ac56a189f4be78b
drkthunder02 <mancuso.christopher@gmail.com>
2019-12-11 23:46:53 -0600

    modified the way characters are looked up in the database versus esi

--- app/Library/Finances/Helper/FinanceHelper.php
+++ app/Library/Finances/Helper/FinanceHelper.php
@@ -138,7 +138,6 @@ class FinanceHelper {
 
         //Refrence to see if the character is in our look up table for corporation and characters
         $char = $lookup->GetCharacterInfo($charId);
-        dd($char);
         $corpId = $char->corporation_id;
 
         //Create the ESI authentication container
