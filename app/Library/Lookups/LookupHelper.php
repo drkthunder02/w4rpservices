d20f59313de7d147639ebc5858b8fd7562e75425
drkthunder02 <mancuso.christopher@gmail.com>
2019-12-12 00:09:04 -0600

    test controller

--- app/Library/Lookups/LookupHelper.php
+++ app/Library/Lookups/LookupHelper.php
@@ -151,7 +151,7 @@ class LookupHelper {
         $char = $this->LookupCharacter($charId, null);
 
         //if the character was not found in the database, then get the information and store it in our database for later
-        if(!isset($char)) {
+        if($char === null) {
 
             try {
                 $response = $this->esi->invoke('get', '/characters/{character_id}/', [
