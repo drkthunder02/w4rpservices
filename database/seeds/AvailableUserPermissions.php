308164f7f4ef388913828708629c7257741466de
drkthunder02 <mancuso.christopher@gmail.com>
2019-12-12 18:05:27 -0600

    cleaned up AvailableUserPermissions seeder

--- database/seeds/AvailableUserPermissions.php
+++ database/seeds/AvailableUserPermissions.php
@@ -19,20 +19,5 @@ class AvailableUserPermissions extends Seeder
             'permission' => 'logistics.minion',
         ]);
 
-        DB::table('available_user_permissions')->insert([
-            'permission' => 'helpdesk.diplomat',
-        ]);
-
-        DB::table('available_user_permissions')->insert([
-            'permission' => 'helpdesk.moonadmin',
-        ]);
-
-        DB::table('available_user_permissions')->insert([
-            'permission' => 'helpdesk.fleetcommand',
-        ]);
-
-        DB::table('available_user_permissions')->insert([
-            'permission' => 'helpdesk.leadership',
-        ]);
     }
 }
