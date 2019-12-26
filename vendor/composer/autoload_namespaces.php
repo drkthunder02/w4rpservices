fcfff0be06e5af2276940b7c206a0296b988ee6a
drkthunder02 <mancuso.christopher@gmail.com>
2019-12-26 05:03:41 +0000

    tracking stuff

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -6,6 +6,9 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Snowplow\\RefererParser' => array($vendorDir . '/snowplow/referer-parser/php/src'),
     'Parsedown' => array($vendorDir . '/erusev/parsedown'),
     'Mockery' => array($vendorDir . '/mockery/mockery/library'),
+    'Detection' => array($vendorDir . '/mobiledetect/mobiledetectlib/namespaced'),
+    'Bllim\\Datatables' => array($vendorDir . '/pragmarx/datatables/src'),
 );
