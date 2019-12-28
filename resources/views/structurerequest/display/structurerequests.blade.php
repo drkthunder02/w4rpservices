dc042d982c966049fd42c6939dac71e5a2f8b399
drkthunder02 <mancuso.christopher@gmail.com>
2019-12-28 15:03:04 -0600

    structure request form

--- resources/views/structurerequest/display/structurerequests.blade.php
+++ resources/views/structurerequest/display/structurerequests.blade.php
@@ -21,6 +21,7 @@
                 @if($reqs != null)
                 @foreach($reqs as $req)
                     <tr>
+                        {{ dd($req->id) }}
                         <td>{{ $req->corporation_name }}</td>
                         <td>{{ $req->system }}</td>
                         <td>{{ $req->structure_size }}</td>
