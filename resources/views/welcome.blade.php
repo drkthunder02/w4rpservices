3f782dfc368f4bbfdbe5744d92a62ab9b7a822a2
drkthunder02 <mancuso.christopher@gmail.com>
2020-01-30 20:59:20 -0600

    image for main page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
                 <div class="card-header">Welcome to the W4RP Services Page</div>
                 <div class="card-body">
                     <div class="container" align="center">
-                        <a href="/login" img={{'/img/eve-soo-login-white-large.png'}}>Login</a>
+                        <a href="/login" img={{'img/eve-soo-login-white-large.png'}}>Login</a>
                     </div>
                 </div>
             </div>
