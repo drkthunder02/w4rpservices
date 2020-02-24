384a875945cc4d1b1604838df486d269ccefecb7
drkthunder02 <mancuso.christopher@gmail.com>
2020-02-24 00:26:31 -0600

    moons controller

--- resources/views/moons/user/allmoons.blade.php
+++ resources/views/moons/user/allmoons.blade.php
@@ -3,18 +3,13 @@
 <div class="container">
 <div class="row">
     <h2>Moons in W4RP Space</h2>
-    <ul class="nav nav-pills nav-fill">
+    <ul class="nav nav-pills">
         @foreach($systems as $system)
         <li>
             <a data-toggle="pill" href="#W4RP-{{$system}}">{{$system}}</a>
         </li>
         @endforeach
     </ul>
-    <nav class="nav nav-pills nav-fill">
-        @foreach($systems as $system)
-        <a data-toggle="pill" href="#W4RP-{{$system}}">{{$system}}</a>
-        @endforeach
-    </nav>
     <div class="tab-content">
         @foreach($systems as $system)
             <div id="W4RP-{{ $system }}" class="tab-pane fade">
@@ -63,12 +58,4 @@
     </div>
 </div>
 </div>
-<script> 
-    $(document).ready(function() { 
-        $('nav a').click(function() { 
-            $('nav a').removeClass("active"); 
-            $(this).addClass("active"); 
-        }); 
-    }); 
-</script> 
 @endsection
