f2f427cce6970b8394e408adfe161e301372779a
drkthunder02 <mancuso.christopher@gmail.com>
2020-02-24 00:11:27 -0600

    moons controller

--- resources/views/layouts/moons/b4.blade.php
+++ resources/views/layouts/moons/b4.blade.php
@@ -18,12 +18,9 @@
 
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
-        <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
        
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-        <!--<link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}"> -->
+        <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
     </head>
     <body>
         @include('layouts.navbar')
