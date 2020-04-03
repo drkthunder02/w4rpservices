63f1f93ddd72025d241d7bd6bac1aa74edd2e55f
drkthunder02 <mancuso.christopher@gmail.com>
2020-04-03 01:15:47 -0500

    new admin dashboard

--- resources/views/layouts/admin/topnavbar.blade.php
+++ resources/views/layouts/admin/topnavbar.blade.php
@@ -1,10 +1,10 @@
 <nav class="navbar navbar-light fixed-top bg-light flex-md-nowrap p-0 shadow">
     <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">W4RP</a>
     <ul class="navbar-nav px-3">
-        <li class="nav-item text-nowrap">
+        <li class="nav-item">
             <a class="nav-link" href="/dashboard">Dashboard</a>
         </li>
-        <li class="nav-item text-nowrap">
+        <li class="nav-item">
             <a class="nav-link" href="/logout">Logout</a>
         </li>
     </ul>
