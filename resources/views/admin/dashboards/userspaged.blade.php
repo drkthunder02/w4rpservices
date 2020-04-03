583ceb1427f1b2b016cebf059a3da64a75eedbe4
drkthunder02 <mancuso.christopher@gmail.com>
2020-04-03 01:43:44 -0500

    updated dashboard.css

--- resources/views/admin/dashboards/userspaged.blade.php
+++ resources/views/admin/dashboards/userspaged.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.admin.b4')
 @section('content')
-<div class="container-fluid px-100">
+<div class="container-fluid w-50">
     <div class="card">
         <div class="card-header">
             <h2>User Information</h2>
