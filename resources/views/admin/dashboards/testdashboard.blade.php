fd4dcf88a9edca506354b291ec87a00710b74cd1
drkthunder02 <mancuso.christopher@gmail.com>
2020-04-03 01:52:09 -0500

    admin test dashboard

--- resources/views/admin/dashboards/testdashboard.blade.php
+++ resources/views/admin/dashboards/testdashboard.blade.php
@@ -2,6 +2,30 @@
 @section('content')
 <div class="container">
 <h2>Admin Dashboard Test</h2><br>
-Just a test of the new admin dashboard and how it should look with a sidebar navigation system.
+<div class="card">
+    <div class="card-header">
+        <h2>user Information</h2>
+    </div>
+    <div class="card-body">
+        Form<br>
+        <table class="table table-striped table-bordered">
+            <thead>
+                <th>Name</th>
+                <th>Role</th>
+                <th>Permissions</th>
+                <th>Action</th>
+            </thead>
+            <tbody>
+                @for ($i = 0; $i < 50; $i++)
+                <tr>
+                    <td>Name</td>
+                    <td>Role</td>
+                    <td>Permissions</td>
+                    <td>Form</td>
+                </tr> 
+                @endfor
+            </tbody>
+        </table>
+    </div>
 </div>
 @endsection
