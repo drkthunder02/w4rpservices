1e47a3cbafa50f902c0f8f74a7a08a280be2630f
drkthunder02 <mancuso.christopher@gmail.com>
2020-04-03 00:53:06 -0500

    test admin dashboard

--- resources/views/admin/dashboards/testdashboard.blade.php
+++ resources/views/admin/dashboards/testdashboard.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.admin.b4')
 @section('content')
+<br>
 <div class="container">
+<br>
 <h2>Admin Dashboard Test</h2><br>
 Just a test of the new admin dashboard and how it should look with a sidebar navigation system.
 </div>
