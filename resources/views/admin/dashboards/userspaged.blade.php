fc0f2686e6fb3a074fc407d752c25a55045286e3
drkthunder02 <mancuso.christopher@gmail.com>
2020-04-03 01:55:17 -0500

    admin test dashboard

--- resources/views/admin/dashboards/userspaged.blade.php
+++ resources/views/admin/dashboards/userspaged.blade.php
@@ -1,48 +1,46 @@
-@extends('admin.layouts.b4')
+@extends('layouts.admin.b4')
 @section('content')
-<div class="container-fluid">
-    <div class="card">
-        <div class="card-header">
-            <h2>User Information</h2>
-        </div>
-        <div class="card-body">
-            <div class="container">
-                {!! Form::open(['action' => 'Dashboard\AdminController@searchUsers', 'method' => 'POST']) !!}
-                <div class="form-group">
-                    {{ Form::label('parameter', 'Seach For A User') }}
-                    {{ Form::text('parameter', '', ['class' => 'form-control', 'placeholder' => 'CCP Antiquarian']) }}
-                </div>
-                {{ Form::submit('Search', ['class' => 'btn btn-primary']) }}
-                {!! Form::close() !!}
+<div class="card">
+    <div class="card-header">
+        <h2>User Information</h2>
+    </div>
+    <div class="card-body">
+        <div class="container">
+            {!! Form::open(['action' => 'Dashboard\AdminController@searchUsers', 'method' => 'POST']) !!}
+            <div class="form-group">
+                {{ Form::label('parameter', 'Seach For A User') }}
+                {{ Form::text('parameter', '', ['class' => 'form-control', 'placeholder' => 'CCP Antiquarian']) }}
             </div>
-            <table class="table table-striped table-bordered">
-                <thead>
-                    <th>Name</th>
-                    <th>Role</th>
-                    <th>Permissions</th>
-                    <th>Action</th>
-                </thead>
-                <tbody>
-                    @foreach($usersArr as $user)
-                    <tr>
-                        <td>{{ $user->name }}</td>
-                        <td>{{ $user->role }}</td>
-                        <td>{{ $user->permission }}</td>
-                        <td>
-                            {!! Form::open(['action' => 'Dashboard\AdminController@displayModifyUser', 'method' => 'POST']) !!}
-                            {{ Form::hidden('user', $user->name) }}
-                            {{ Form::submit('Modify User', ['class' => 'btn btn-primary']) }}
-                            {!! Form::close() !!}
-                            {!! Form::open(['action' => 'Dashboard\AdminController@removeUser', 'method' => 'POST']) !!}
-                            {{ Form::hidden('user', $user->name) }}
-                            {{ Form::submit('Remove User', ['class' => 'btn btn-danger']) }}
-                            {!! Form::close() !!}
-                        </td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
+            {{ Form::submit('Search', ['class' => 'btn btn-primary']) }}
+            {!! Form::close() !!}
         </div>
+        <table class="table table-striped table-bordered">
+            <thead>
+                <th>Name</th>
+                <th>Role</th>
+                <th>Permissions</th>
+                <th>Action</th>
+            </thead>
+            <tbody>
+                @foreach($usersArr as $user)
+                <tr>
+                    <td>{{ $user->name }}</td>
+                    <td>{{ $user->role }}</td>
+                    <td>{{ $user->permission }}</td>
+                    <td>
+                        {!! Form::open(['action' => 'Dashboard\AdminController@displayModifyUser', 'method' => 'POST']) !!}
+                        {{ Form::hidden('user', $user->name) }}
+                        {{ Form::submit('Modify User', ['class' => 'btn btn-primary']) }}
+                        {!! Form::close() !!}
+                        {!! Form::open(['action' => 'Dashboard\AdminController@removeUser', 'method' => 'POST']) !!}
+                        {{ Form::hidden('user', $user->name) }}
+                        {{ Form::submit('Remove User', ['class' => 'btn btn-danger']) }}
+                        {!! Form::close() !!}
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
 </div>
 <br>
