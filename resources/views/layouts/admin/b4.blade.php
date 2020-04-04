896687f7357d7bfed0ff6119789194c8e357dee8
drkthunder02 <mancuso.christopher@gmail.com>
2020-04-03 20:40:33 -0500

    removed dashboard css as it was driving me nuts.

--- resources/views/layouts/admin/b4.blade.php
+++ resources/views/layouts/admin/b4.blade.php
@@ -24,8 +24,6 @@
         -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
-        <!-- Dashboard Custom CSS -->
-        <link rel="stylesheet" href="{{ asset('css/admin/dashboard.css') }}">
     </head>
     <body>
         <!-- Top Navbar -->
