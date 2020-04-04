1b88380e38d07ec261d526d08fc4e77dffe1161a
drkthunder02 <mancuso.christopher@gmail.com>
2020-04-03 23:35:39 -0500

    admin lte

--- resources/views/layouts/admin/testb4.blade.php
+++ resources/views/layouts/admin/testb4.blade.php
@@ -67,8 +67,6 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="#" class="brand-link">
-      <img src="/bower_components/admin-lte/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
-           style="opacity: .8">
       <span class="brand-text font-weight-light">W4RP</span>
     </a>
 
