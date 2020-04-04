7bdbc95a19cca6504a390d5f526859c83468bd40
drkthunder02 <mancuso.christopher@gmail.com>
2020-04-03 23:51:31 -0500

    admin lte

--- resources/views/layouts/admin/testb4.blade.php
+++ resources/views/layouts/admin/testb4.blade.php
@@ -220,8 +220,10 @@
   <aside class="control-sidebar control-sidebar-dark">
     <!-- Control sidebar content goes here -->
     <div class="p-3">
-      <h5>Title</h5>
-      <p>Sidebar content</p>
+      <h5>Admin Dashboard</h5>
+      <p>
+          The admin dashboard holds all the functionality for the administrators of the site.  This tab is currently not utilized.
+      </p>
     </div>
   </aside>
   <!-- /.control-sidebar -->
