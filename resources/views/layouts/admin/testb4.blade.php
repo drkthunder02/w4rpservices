81b0634e1bab307238f15a1a29f970daabb5f565
drkthunder02 <mancuso.christopher@gmail.com>
2020-04-03 23:34:56 -0500

    admin lte

--- resources/views/layouts/admin/testb4.blade.php
+++ resources/views/layouts/admin/testb4.blade.php
@@ -201,15 +201,6 @@
               </li>
             </ul>
           </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-th"></i>
-              <p>
-                Simple Link
-                <span class="right badge badge-danger">New</span>
-              </p>
-            </a>
-          </li>
         </ul>
       </nav>
       <!-- /.sidebar-menu -->
