a259764743588a43a8466190e1c8fd3e81f5d047
drkthunder02 <mancuso.christopher@gmail.com>
2020-04-03 20:08:20 -0500

    side nav bar for admin dashboard

--- resources/views/admin/dashboards/dashboard.blade.php
+++ resources/views/admin/dashboards/dashboard.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.admin.b4')
 @section('content')
-<div class="container">
+<div class="container-fluid">
     <div class="card">
         <div class="card-header">
             <h2>Admin Dashboard</h2>
--- resources/views/layouts/admin/topnavbar.blade.php
+++ resources/views/layouts/admin/topnavbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-light fixed-top bg-light flex-md-nowrap p-0 shadow">
     <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">W4RP</a>
-    <ul class="navbar-nav px-3">
+    <ul class="navbar-nav">
         <li class="nav-item text-nowrap">
             <a class="nav-link" href="/dashboard">Dashboard</a>
         </li>
