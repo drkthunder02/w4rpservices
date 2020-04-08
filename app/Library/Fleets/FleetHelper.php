f88232fdd3ca2d9d55a90075f1a4e27c126a0e21
drkthunder02 <mancuso.christopher@gmail.com>
2020-04-08 00:21:12 -0500

    created layout for fleet helper

--- app/Library/Fleets/FleetHelper.php
+++ app/Library/Fleets/FleetHelper.php
@@ -16,6 +16,52 @@ use Seat\Eseye\Exceptions\RequestFailedException;
 use App\library\Esi\Esi;
 
 //Models
+use App\Models\Fleets\AllianceFleet;
+use App\Models\Fleets\AllianceFleetMember;
 
+class FleetHelper {
+    //Variables
+    private $esi;
+
+    //Constructi
+    public function __construct($charId) {
+        //Declare a variable for use by the constructor
+        $esiHelper = new Esi;
+
+        //Check for the ESI scope
+        $check = $esiHelper->HaveEsiScope($charId, 'esi-fleets.read_fleet.v1');
+        if($check) {
+            //Setup the ESI authentication container
+            $this->esi = $esiHelper->SetupEsiAuthentication();
+        } else {
+            $this->esi = null;
+        }
+    }
+
+    //Get fleet information
+    public function GetFleetInfo($fleetId) {
+
+    }
+
+    //Get fleet members
+    public function GetFleetMembers($fleetId) {
+
+    }
+
+    //Get fleet wings
+    public function GetFleetWings($fleetId) {
+
+    }
+
+    //Update fleet time
+    public function UpdateFleetTime($fleetId) {
+
+    }
+
+    //Update fleet character names
+    public function UpdateFleetCharacters($fleetId) {
+        
+    }
+}
 
 ?>
