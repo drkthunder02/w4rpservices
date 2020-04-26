b71e44bd013ec80b0e21dcb16b6598ca93455840
drkthunder02 <mancuso.christopher@gmail.com>
2020-04-26 00:32:50 -0500

    mining ledger testing

--- resources/views/moons/ledger/corpmoons.blade.php
+++ resources/views/moons/ledger/corpmoons.blade.php
@@ -28,7 +28,6 @@
                     </thead>
                     <tbody>
                         @foreach($miningLedgers as $ledger)
-                        {{dd($ledger)}}
                             @if($ledger['structure'] == $value)
                             <tr>
                                 <td>{{ $ledger['character'] }}</td>
