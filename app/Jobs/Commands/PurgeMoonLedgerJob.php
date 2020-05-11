e855350b7fd318645c47ec6893ac4893c61d7df2
drkthunder02 <mancuso.christopher@gmail.com>
2020-05-10 19:16:37 -0500

    finished the purge moon ledger job

--- app/Jobs/Commands/PurgeMoonLedgerJob.php
+++ app/Jobs/Commands/PurgeMoonLedgerJob.php
@@ -2,15 +2,35 @@
 
 namespace App\Jobs\Commands;
 
+//Internal Libraries
 use Illuminate\Bus\Queueable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
+use Log;
+use Carbon\Carbon;
+
+//App Models
+use App\Models\CorpMoonLedger;
 
 class PurgeMoonLedgerJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Timeout in seconds
+     * 
+     * @var int
+     */
+    public $timeout = 3600;
+
+    /**
+     * Retries
+     * 
+     * @var int
+     */
+    public $retries = 3;
 
     /**
      * Create a new job instance.
@@ -29,6 +49,9 @@ class PurgeMoonLedgerJob implements ShouldQueue
      */
     public function handle()
     {
-        //
+        $previous = Carbon::now()->subDays(60);
+
+        //Remove old ledger entries
+        CorpMoonLedger::where('created_at', '<', $previous)->delete();
     }
 }
