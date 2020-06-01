6853ae9881e363f3f2778f3e3b9fe52941f6206e
drkthunder02 <mancuso.christopher@gmail.com>
2020-06-01 02:50:28 -0500

    created outline for payment reminder

--- app/Jobs/Commands/RentalMoons/SendMoonRentalPaymentReminderJob.php
+++ app/Jobs/Commands/RentalMoons/SendMoonRentalPaymentReminderJob.php
@@ -8,7 +8,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 
-class SendMoonRentalReminderJob implements ShouldQueue
+class SendMoonRentalPaymentReminderJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
