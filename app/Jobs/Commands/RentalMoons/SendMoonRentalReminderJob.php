14b59ed4188cbf6f62fc375e229d254229ef8eae
drkthunder02 <mancuso.christopher@gmail.com>
2020-06-01 02:50:08 -0500

    created outline for payment reminder

---
+++ app/Jobs/Commands/RentalMoons/SendMoonRentalReminderJob.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Jobs\Commands\RentalMoons;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class SendMoonRentalReminderJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Timeout in seconds
+     * 
+     * @var int
+     */
+    public $timeout = 1600;
+
+    /**
+     * Retries
+     * 
+     * @var int
+     */
+    public $retries = 3;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //Set the queue connection
+        $this->connection = 'redis';
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        //
+    }
+}
