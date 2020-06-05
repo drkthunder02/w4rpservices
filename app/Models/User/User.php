b9aab0981e61b517b088258cb8bd33cc100ba223
drkthunder02 <mancuso.christopher@gmail.com>
2020-06-05 01:35:49 -0500

    rental moon testing

--- app/Models/User/User.php
+++ app/Models/User/User.php
@@ -121,7 +121,7 @@ class User extends Authenticatable
     }
 
     public function isMoonRenter() {
-        $moonRental = MoonRental::where([
+        $moonRental = AllianceMoonRental::where([
             'Contact' => $this->character_id,
         ])->first();
 
