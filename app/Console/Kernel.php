d1308b6908dc9ef1279ab75abe49664f547cbef0
drkthunder02 <mancuso.christopher@gmail.com>
2020-06-05 01:54:53 -0500

    rental moon testing

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -46,46 +46,43 @@ class Kernel extends ConsoleKernel
         //Horizon Graph Schedule
         $schedule->command('horizon:snapshot')->everyFiveMinutes();
 
-        //Command to get the holding journal finances
+        /**
+         * Rentals / Flex Schedule
+         */
+        $schedule->command('services:UpdateRentalPrice')
+                 ->hourlyAt('15')
+                 ->withoutOverlapping();
+        $schedule->command('services:FlexStructures')
+                 ->monthlyOn(2, '00:01');
+
+        /**
+         * Holding Corp Finance Schedule
+         */
         $schedule->command('services:HoldingJournal')
                 ->hourlyAt('45')
                 ->withoutOverlapping();
 
-        //Get the corps within the alliance
+        /**
+         * Get Information Schedule
+         */
         $schedule->command('services:GetCorps')
                  ->monthlyOn(1, '09:00');
-
-        //Get the structures within the alliance and their information
         $schedule->command('services:GetStructures')
                 ->dailyAt('09:00');
-
-        //Get the assets for the alliance.  Only saves the Liquid Ozone for jump bridges
         $schedule->command('services:GetAssets')
-                ->hourlyAt('22');
+                ->hourlyAt('22');        
 
-        //Clean old data from the database
-        $schedule->command('services:CleanData')
-                ->monthlyOn(15, '18:00');
-
-        //Purge users from the database which don't belong and reset the user roles as necessary
-        $schedule->command('services:PurgeUsers')
-                ->dailyAt('23:00');
-
-        //Mail about payments for flex structures
-        $schedule->command('services:FlexStructures')
-                ->monthlyOn(2, '00:01');
-
-        //Wormhole data purge
+        /**
+         * Purge Data Schedule
+         */
         $schedule->command('services:PurgeWormholeData')
                 ->hourlyAt(20);
-
-        //Purge old data from the database
         $schedule->command('services:CleanData')
                  ->weekly(7, '11:00');
-
-        //Purge old corp ledger data from the database
         $schedule->command('data:PurgeCorpLedgers')
                  ->monthly();
+        $schedule->command('services:PurgeUsers')
+                 ->dailyAt('23:00');
     }
 
     /**
