13be1935912d7ec9fb48a8732ff26c6550e8456b
drkthunder02 <mancuso.christopher@gmail.com>
2020-06-06 04:49:22 -0500

    rental moon testing

--- resources/views/moons/user/moon.blade.php
+++ resources/views/moons/user/moon.blade.php
@@ -6,13 +6,9 @@
                 <th>System</th>
                 <th>Name</th>
                 <th>First Ore</th>
-                <th>Quantity</th>
                 <th>Second Ore</th>
-                <th>Quantity</th>
                 <th>Third Ore</th>
-                <th>Quantity</th>
                 <th>Fourth Ore</th>
-                <th>Quantity</th>
                 <th>Rental Price</th>
                 <th>Moon Worth</th>
                 <th>Rental End</th>
@@ -22,14 +18,10 @@
                 <tr class="{{ $row['RowColor'] }}">
                     <td>{{ $row['SPM'] }}</td>
                     <td>{{ $row['StructureName'] }}</td>
-                    <td>{{ $row['FirstOre'] }}</td>
-                    <td>{{ $row['FirstQuantity'] }}</td>
-                    <td>{{ $row['SecondOre'] }}</td>
-                    <td>{{ $row['SecondQuantity'] }}</td>
-                    <td>{{ $row['ThirdOre'] }}</td>
-                    <td>{{ $row['ThirdQuantity'] }}</td>
-                    <td>{{ $row['FourthOre'] }}</td>
-                    <td>{{ $row['FourthQuantity'] }}</td>
+                    <td>{{ $row['FirstOre'] }} - {{ $row['FirstQuantity'] }}</td>
+                    <td>{{ $row['SecondOre'] }} - {{ $row['SecondQuantity'] }}</td>
+                    <td>{{ $row['ThirdOre'] }} - {{ $row['ThirdQuantity'] }}</td>
+                    <td>{{ $row['FourthOre'] }} - {{ $row['FourthQuantity'] }}</td>
                     <td>{{ $row['Price'] }}</td>
                     <td>{{ $row['Worth'] }}</td>
                     <td>{{ $row['RentalEnd'] }}</td>
