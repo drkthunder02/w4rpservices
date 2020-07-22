60814f7c1ce9d26289e1f1a228f8dd715eac9966
drkthunder02 <mancuso.christopher@gmail.com>
2020-07-22 01:03:25 -0500

    added buttons to dashboard for supply chain contracts

--- resources/views/supplychain/dashboard/main.blade.php
+++ resources/views/supplychain/dashboard/main.blade.php
@@ -3,12 +3,30 @@
 <div class="container">
     <div class="row justify-content-center">
         <h2>Supply Chain Contracts</h2>
+        <!-- Create button for creating a new contract -->
+
+        <!-- Create button for deleting own contracts -->
+    </div>
+    <br>
+    <div class="row">
+        <div class="justify-content-left">
+            <a class="btn btn-primary" href="/supplychain/contracts/new" role="button">Create Contract</a>
+        </div>
+        <div class="justify-content-right">
+            <a class="btn btn-danger" href="/supplychain/contracts/delete" role="button">Delete Contract</a>
+        </div>
     </div>
 </div>
 <br>
-@if(count($contracts))
-    @include('supplychain.includes.contracts')
+@if(count($openContracts))
+    @include('supplychain.includes.opencontracts')
 @else
     @include('supplychain.includes.nocontracts')
 @endif
+@if(count($closedContracts))
+    @include('supplychain.includes.closedcontracts')
+@else
+    @include('supplychain.includes.nocontracts')
+@endif
+
 @endsection
