0f108a709c8dc1dcba85be5e721500b12407fbdc
drkthunder02 <mancuso.christopher@gmail.com>
2020-08-07 13:40:24 -0500

    supply chain controller

--- resources/views/supplychain/dashboard/bids.blade.php
+++ resources/views/supplychain/dashboard/bids.blade.php
@@ -8,7 +8,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            @if(count($bidsCount) > 0)
+            @if($bidsCount > 0)
                 @foreach($bids as $bid)
                 <div class="card">
                     <div class="card-header">
