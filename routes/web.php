ed7e2c9bb40c9aba9dbb6a3b66263ed0f38ca5a1
drkthunder02 <mancuso.christopher@gmail.com>
2020-08-12 02:12:49 -0500

    system rentals pages

--- routes/web.php
+++ routes/web.php
@@ -194,11 +194,11 @@ Route::group(['middleware' => ['auth']], function(){
     /**
      * System Rentals Controller display pages
      */
-    Route::get('/system/rental/dashboard', 'RentalSystem\RentalAdminController@displayRentalSystems');
-    Route::get('/system/rental/add', 'RentalSystem\RentalAdminController@displayAddRentalSystem');
-    Route::post('/system/rental/add', 'RentalSystem\RentalAdminController@addRentalSystem');
-    Route::post('/system/rental/update', 'RentalSystem\RentalAdminController@updateRentalSystem');
-    Route::post('/system/rental/remove', 'RentalSystem\RentalAdminController@removeRentalSystem');
+    Route::get('/system/rental/dashboard', 'SystemRentals\RentalAdminController@displayRentalSystems');
+    Route::get('/system/rental/add', 'SystemRentals\RentalAdminController@displayAddRentalSystem');
+    Route::post('/system/rental/add', 'SystemRentals\RentalAdminController@addRentalSystem');
+    Route::post('/system/rental/update', 'SystemRentals\RentalAdminController@updateRentalSystem');
+    Route::post('/system/rental/remove', 'SystemRentals\RentalAdminController@removeRentalSystem');
 
     /**
      * Test Controller display pages
