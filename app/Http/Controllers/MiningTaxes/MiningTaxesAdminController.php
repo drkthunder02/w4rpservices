81783278287bfb995f4a5a24259e9c754edf9cbe
drkthunder02 <mancuso.christopher@gmail.com>
2021-02-11 12:58:37 +0900

    framework for mining taxes admin controller

--- app/Http/Controllers/MiningTaxes/MiningTaxesAdminController.php
+++ app/Http/Controllers/MiningTaxes/MiningTaxesAdminController.php
@@ -7,5 +7,44 @@ use Illuminate\Http\Request;
 
 class MiningTaxesAdminController extends Controller
 {
-    //
+    public function __construct() {
+        $this->middleware('auth');
+        $this->middleware('role:Admin');
+    }
+
+    /**
+     * Display current unpaid invoices
+     */
+    public function DisplayUnpaidInvoice() {
+
+    }
+
+    /**
+     * Mark an invoice paid
+     */
+    public function UpdateInvoice() {
+
+    }
+
+    /**
+     * Delete an invoice and mark items paid
+     */
+    public function DeleteInvoice() {
+        
+    }
+
+    /**
+     * Display past paid invoices
+     */
+    public function DisplayPaidInvoices() {
+
+    }
+
+    /**
+     * Display admin mining ledgers by month
+     */
+    public function DisplayMonthlyMiningLedgers() {
+
+    }
+    
 }
