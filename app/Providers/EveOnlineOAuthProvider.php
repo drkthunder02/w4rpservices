75cc0b23553114a48f4b6372fe4364dd97cce6f1
drkthunder02 <mancuso.christopher@gmail.com>
2021-02-12 04:34:05 +0900

    map user object testing

--- app/Providers/EveOnlineOAuthProvider.php
+++ app/Providers/EveOnlineOAuthProvider.php
@@ -51,12 +51,26 @@ class EveOnlineOAuthProvider extends AbstractProvider implements ProviderInterfa
      * @return \Laravel\Socialite\Two\User
      */
     protected function mapUserToObject(array $user) {
+
+        dd($user);
+
         return (new User)->setRaw($user)->map([
             'id' => $user['CharacterID'],
             'name' => $user['CharacterName'],
             'owner_hash' => $user['CharacterOwnerHash'],
             'avatar' => 'https://image.eveonline.com/Character/' . $user['CharacterID'] . '_128.jpg',
         ]);
+        /*
+        return (new User)->setRaw($user)->map([
+            'id'                   => $character_id,
+            'name'                 => $user['name'],
+            'nickname'             => $user['name'],
+            'character_owner_hash' => $user['owner'],
+            'scopes'               => is_array($user['scp']) ? $user['scp'] : [$user['scp']],
+            'expires_on'           => $user['exp'],
+            'avatar'               => $avatar,
+        ]);
+        */
     }
 
     /**
