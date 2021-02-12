7a63f1370f7d955d21b473d9bf7d5fa5f8f5faae
drkthunder02 <mancuso.christopher@gmail.com>
2021-02-13 03:27:59 +0900

    guzzle

--- app/Library/Esi/Esi.php
+++ app/Library/Esi/Esi.php
@@ -107,6 +107,8 @@ class Esi {
                 $scopesArr = EsiScope::where([
                     'character_id' => $token->character_id,
                 ])->get()->toArray();
+
+                dd($scopesArr);
                 //Create string separated by %20 = space from the array of scopes
                 $scopes = implode(" ", $scopesArr);
                 $scopes = str_replace(" ", "%20", $scopes);
