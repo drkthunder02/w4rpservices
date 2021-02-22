01bff581695ad545ed68c50d8ce0dfd8f1070b25
drkthunder02 <mancuso.christopher@gmail.com>
2021-02-22 22:48:59 +0900

    ledger job

--- app/Console/Commands/MiningTaxes/MiningTaxesLedgers.php
+++ app/Console/Commands/MiningTaxes/MiningTaxesLedgers.php
@@ -94,7 +94,7 @@ class MiningTaxesLedgers extends Command
                 return null;
             }
 
-            $ledgers = json_decode($ledgers);
+            $ledgers = json_decode($response);
 
             foreach($ledgers as $ledger) {
                 //Get some basic information we need to work with
