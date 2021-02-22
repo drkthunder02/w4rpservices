cb3a6b26e91c61c11e44ff39016a775618b6229f
drkthunder02 <mancuso.christopher@gmail.com>
2021-02-22 23:05:17 +0900

    ledger job

--- app/Library/Moons/MoonCalc.php
+++ app/Library/Moons/MoonCalc.php
@@ -239,6 +239,7 @@ class MoonCalc {
         $promethiumPrice = MineralPrice::where(['ItemId' => 16652])->where('Time', '>', $pastTime)->avg('Price');
         $thuliumPrice = MineralPrice::where(['ItemId' => 16653])->where('Time', '>', $pastTime)->avg('Price');
 
+        dd($oreId);
         //Get the name through the lookup table
         $oreName = $lookupHelper->ItemIdToName($oreId);
         dd($oreName);
