a1c6c4fd25fa74f4441a47f2f0401cd6f8c09f54
drkthunder02 <mancuso.christopher@gmail.com>
2021-03-07 22:14:04 +0900

    mining tax testing

--- app/Http/Controllers/MiningTaxes/MiningTaxesController.php
+++ app/Http/Controllers/MiningTaxes/MiningTaxesController.php
@@ -229,7 +229,7 @@ class MiningTaxesController extends Controller
         $config = config();
         $sHelper = new StructureHelper($config['primary'], $config['corporation']);
 
-        dd($config['primary']);
+        dd($config);
 
         $check = EsiScope::where(['character_id' => $config['primary']])->get()->toArray();
         dd($check);
