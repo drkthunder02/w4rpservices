4c513b76251242c8b2ccb5aed58a86c5f11245a7
drkthunder02 <mancuso.christopher@gmail.com>
2021-03-07 21:01:53 +0900

    mining tax testing

--- app/Http/Controllers/MiningTaxes/MiningTaxesController.php
+++ app/Http/Controllers/MiningTaxes/MiningTaxesController.php
@@ -170,7 +170,7 @@ class MiningTaxesController extends Controller
         foreach($extractions as $extract) {
             $calendar->addRow([
                 $esiHelper->DecodeDate($extract->chunk_arrival_time),
-                rand(1,10),
+                true,
             ]);
         }    
                 
