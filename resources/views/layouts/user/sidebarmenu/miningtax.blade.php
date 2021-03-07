860863c8d5c3632ec74d7a87c839efd34a9792e1
drkthunder02 <mancuso.christopher@gmail.com>
2021-03-07 19:29:41 +0900

    navbar issue

--- resources/views/layouts/user/sidebarmenu/miningtax.blade.php
+++ resources/views/layouts/user/sidebarmenu/miningtax.blade.php
@@ -1,34 +1,34 @@
 <li class="nav-item has-treeview">
     <a href="#" class="nav-link">
         <i class="nav-icon far fa-meh-blank"></i>
-        <p>Mining Taxes
-            <i class="right fas fa-angle-left"></li>
+        <p>Mining Taxes<br>
+        <i class="right fas fa-angle-left"></i>
         </p>
     </a>
     <ul class="nav nav-treeview">
-        <li class="nav-item">
-            <a href="/miningtax/display/invoices" class="nav-link">
-                <i class="far fa-circle nav-icon"></i>
-                <p>Display Invoices</p>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a href="/miningtax/display/extractions" class="nav-link">
-                <i class="far fa-circle nav-icon"></i>
-                <p>Upcoming Extractions<p>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a href="/miningtax/display/calendar" class="nav-link">
-                <i class="far fa-circle nav-icon"></i>
-                <p>Calendar</p>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a href="/miningtax/display/ledgers" class="nav-link">
-                <i class="far fa-circle nav-icon"></i>
-                <p>Ledgers</p>
-            </a>
-        </li>
+      <li class="nav-item">
+          <a href="/miningtax/display/invoices" class="nav-link">
+            <i class="far fa-circle nav-icon"></i>
+            <p>Invoices</p>
+          </a>
+      </li>
+      <li class="nav-item">
+          <a href="miningtax/display/extractions" class="nav-link">
+            <i class="far fa-circle nav-icon"></i>
+            <p>Extractions</p>
+          </a>
+      </li>
+      <li class="nav-item">
+          <a href="/miningtax/display/calendar" class="nav-link">
+            <i class="far fa-circle nav-icon"></i>
+            <p>Calendar</p>
+          </a>
+      </li>
+      <li class="nav-item">
+          <a href="/miningtax/display/ledgers" class="nav-link">
+            <i class="far fa-circle nav-icon"></i>
+            <p>Ledgers</p>
+          </a>
+      </li>
     </ul>
-</li>
+  </li>
