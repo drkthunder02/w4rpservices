62b2cc1003c1d7287e37689a51616a4ab19289ea
drkthunder02 <mancuso.christopher@gmail.com>
2021-03-08 18:15:21 +0900

    modified the navbar options for mining taxes on the user side to give unique icons.

--- resources/views/layouts/user/sidebarmenu/miningtax.blade.php
+++ resources/views/layouts/user/sidebarmenu/miningtax.blade.php
@@ -1,6 +1,6 @@
 <li class="nav-item has-treeview">
     <a href="#" class="nav-link">
-        <i class="nav-icon far fa-meh-blank"></i>
+        <i class="nav-icon fas fa-cubes"></i>
         <p>Mining Taxes<br>
         <i class="right fas fa-angle-left"></i>
         </p>
@@ -8,25 +8,25 @@
     <ul class="nav nav-treeview">
       <li class="nav-item">
           <a href="/miningtax/display/invoices" class="nav-link">
-            <i class="far fa-circle nav-icon"></i>
+            <i class="fas fa-cog nav-icon"></i>
             <p>Invoices</p>
           </a>
       </li>
       <li class="nav-item">
           <a href="/miningtax/display/extractions" class="nav-link">
-            <i class="far fa-circle nav-icon"></i>
+            <i class="fas fa-cog nav-icon"></i>
             <p>Extractions</p>
           </a>
       </li>
       <li class="nav-item">
           <a href="/miningtax/display/calendar" class="nav-link">
-            <i class="far fa-circle nav-icon"></i>
+            <i class="fas fa-cog nav-icon"></i>
             <p>Calendar</p>
           </a>
       </li>
       <li class="nav-item">
           <a href="/miningtax/display/ledgers" class="nav-link">
-            <i class="far fa-circle nav-icon"></i>
+            <i class="fas fa-cog nav-icon"></i>
             <p>Ledgers</p>
           </a>
       </li>
