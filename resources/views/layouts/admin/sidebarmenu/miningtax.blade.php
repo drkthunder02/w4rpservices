f642c759e26ea8bd321e2da9337db75a49d05739
drkthunder02 <mancuso.christopher@gmail.com>
2021-03-08 18:12:29 +0900

    modified the navbar options for mining taxes on the user side to give unique icons.

--- resources/views/layouts/admin/sidebarmenu/miningtax.blade.php
+++ resources/views/layouts/admin/sidebarmenu/miningtax.blade.php
@@ -1,19 +1,19 @@
 <li class="nav-item has-treeview">
     <a href="#" class="nav-link">
-        <i class="nav-icon far fa-meh-blank"></i>
+        <i class="nav-icon fas fa-cubes"></i>
         <p>Mining Taxes</p>
         <i class="right fas fa-angle-left"></i>
     </a>
     <ul class="nav nav-treeview">
         <li class="nav-item">
             <a href="/miningtax/admin/display/unpaid" class="nav-link">
-                <i class="far fa-circle nav-icon"></i>
+                <i class="far fa-money-bill-alt nav-icon"></i>
                 <p>Unpaid Invoices</p>
             </a>
         </li>
         <li class="nav-item">
             <a href="/miningtax/admin/display/paid" class="nav-link">
-                <i class="far fa-circle nav-icon"></i>
+                <i class="far fa-money-bill-alt nav-icon"></i>
                 <p>Paid Invoices</p>
             </a>
         </li>
