d44edffa1b6f766ec05a611d2f980663dee94b21
drkthunder02 <mancuso.christopher@gmail.com>
2021-03-10 22:36:31 +0900

    redirectTo when not logged in.

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -15,7 +15,7 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if(!$this->auth->check()){
-            return redirect()->route('notloggedin');
+            return redirect('/');
         }
     }
 }
