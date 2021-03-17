ce99ad9cf9e2f009e86efee5edab89b3043f27c0
drkthunder02 <mancuso.christopher@gmail.com>
2021-03-18 00:08:40 +0900

    invoice stuff

--- app/Console/Commands/MiningTaxes/MiningTaxesInvoices.php
+++ app/Console/Commands/MiningTaxes/MiningTaxesInvoices.php
@@ -76,6 +76,8 @@ class MiningTaxesInvoices extends Command
                 'invoiced' => 'No',
             ])->get()->toArray();
 
+            dd($rows);
+
             //Taly up the item composition from each row and multiply by the quantity
             foreach($rows as $row) {
                 if(!isset($ores[$row['type_id']])) {
