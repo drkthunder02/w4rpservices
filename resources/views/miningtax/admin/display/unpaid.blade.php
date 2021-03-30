f9b4e9a8985aae235356c18e546d6d023fc3f053
drkthunder02 <mancuso.christopher@gmail.com>
2021-03-30 21:20:21 +0900

    moved links on unpaid invoices page

--- resources/views/miningtax/admin/display/unpaid.blade.php
+++ resources/views/miningtax/admin/display/unpaid.blade.php
@@ -43,6 +43,7 @@
                     @endforeach
                 </tbody>
             </table>
+            {{ $invoices->links() }}
             <br>
             <h2>Total Amount Owed: {{ number_format($totalAmount, 2, ".", ",") }}</h2>
         </div>
