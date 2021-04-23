bd86e1ab05c891b09301d607b6c149c57b8cfcbb
drkthunder02 <mancuso.christopher@gmail.com>
2021-04-24 01:31:45 +0900

    miningt axes payemnts command

--- app/Console/Commands/MiningTaxes/ExecuteProcesssMiningTaxesPaymentsCommand.php
+++ app/Console/Commands/MiningTaxes/ExecuteProcesssMiningTaxesPaymentsCommand.php
@@ -4,7 +4,7 @@ namespace App\Console\Commands\MiningTaxes;
 
 use Illuminate\Console\Command;
 
-use App\Jobs\Commands\MiningTaxes\ProcessMiningTaxesPayemnts as PMTP;
+use App\Jobs\Commands\MiningTaxes\ProcessMiningTaxesPayments as PMTP;
 
 class ExecuteProcesssMiningTaxesPaymentsCommand extends Command
 {
