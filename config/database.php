cd96aa837756bcf90e1baaebab1456c04aa93b0a
drkthunder02 <mancuso.christopher@gmail.com>
2021-05-18 21:20:08 +0900

    redis

--- config/database.php
+++ config/database.php
@@ -106,7 +106,7 @@ return [
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', 6379),
-            'database' => env('REDIS_DB', 0),
+            'database' => env('REDIS_DATABASE', 0),
             //`prefix` => `d:`,
         ],
 
--- config/queue.php
+++ config/queue.php
@@ -38,7 +38,7 @@ return [
             'driver' => 'database',
             'table' => 'jobs',
             'queue' => 'default',
-            'retry_after' => 30,
+            'retry_after' => 60,
         ],
 
         'beanstalkd' => [
@@ -60,7 +60,7 @@ return [
         'redis' => [
             'driver' => 'redis',
             'connection' => 'default',
-            'queue' => 'default',
+            'queue' => env('REDIS_QUEUE', 'default'),
             'retry_after' => 90,
             'block_for' => null,
             'prefix' => env('QUEUE_PREFIX','w4rpservices_queue'),
