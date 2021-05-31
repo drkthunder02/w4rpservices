5d5b23b25b8d0006e5cfe9f30b668f0c2bcfe18a
drkthunder02 <mancuso.christopher@gmail.com>
2021-05-31 23:23:54 +0900

    moon displays

--- app/Http/Controllers/MiningTaxes/MiningTaxesController.php
+++ app/Http/Controllers/MiningTaxes/MiningTaxesController.php
@@ -120,6 +120,8 @@ class MiningTaxesController extends Controller
             ]);
         }
 
+        dd($moons);
+
         return view('miningtax.user.display.moons.allmoons')->with('moons', $moons)
                                                             ->with('systems', $systems)
                                                             ->with('r4Goo', $r4Goo)
