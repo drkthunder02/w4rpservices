9462aea45f0d6630ee4b1a6c5adcd460b6d789ab
Christopher Mancuso <mancuso.christopher@gmail.com>
2021-07-05 21:49:23 -0500

    working on moon rental worth

--- app/Console/Commands/MoonRental/ExecuteUpdateAllianceMoonRentalWorth.php
+++ app/Console/Commands/MoonRental/ExecuteUpdateAllianceMoonRentalWorth.php
@@ -64,17 +64,37 @@ class ExecuteUpdateAllianceMoonRentalWorth extends Command
         foreach($moons as $moon) {
             //Declare the arrays needed
             $ores = array();
+            $worth = 0.00;
 
             $ores = AllianceMoonOre::where([
                 'moon_id' => $moon->moon_id,
             ])->get(['ore_name', 'quantity'])->toArray();
 
+            if(sizeof($ores) == 1) {
+                $ores[1]["ore_name"] = null;
+                $ores[1]["quantity"] = 0.00;
+                $ores[2]["ore_name"] = null;
+                $ores[2]["quantity"] = 0.00;
+                $ores[3]["ore_name"] = null;
+                $ores[3]["quantity"] = 0.00;
+            } else if(sizeof($ores) == 2) {
+                $ores[2]["ore_name"] = null;
+                $ores[2]["quantity"] = 0.00;
+                $ores[3]["ore_name"] = null;
+                $ores[3]["quantity"] = 0.00;
+            } else if(sizeof($ores) == 3) {
+                $ores[3]["ore_name"] = null;
+                $ores[3]["quantity"] = 0.00;
+            }
+
             dd($ores);
 
             //one of these two ways will work
-            $worth = $mHelper->MoonTotalWorth($ores[0][0], $ores[0][1], $ores[1][0], $ores[1][1], $ores[2][0], $ores[2][1], $ores[3][0], $ores[3][1]);
+            $worth = $mHelper->MoonTotalWorth($ores[0]["ore_name"], $ores[0]["quantity"], 
+                                              $ores[1]["ore_name"], $ores[1]["quantity"], 
+                                              $ores[2]["ore_name"], $ores[2]["quantity"], 
+                                              $ores[3]["ore_name"], $ores[3]["quantity"]);
             dd($worth);
-            $worth = $mHelper->MoonTotalWorth($ores[0], $ores[1], $ores[2], $ores[3], $ores[4], $ores[5], $ores[6], $ores[7]);
             $rentalAmount = $worth * $rentalTax * $months;
 
             AllianceMoon::where([
