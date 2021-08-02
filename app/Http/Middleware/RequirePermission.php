99ff081b05d718310b03275ea33c21152d59a339
drkthunder02 <mancuso.christopher@gmail.com>
2021-08-02 01:31:11 -0500

    updated how require permission works

--- app/Http/Middleware/RequirePermission.php
+++ app/Http/Middleware/RequirePermission.php
@@ -26,9 +26,7 @@ class RequirePermission
             $perms = UserPermission::where(['character_id' => auth()->user()->character_id, 'permission'=> $permission])->get(['permission']);
 
             abort_unless(auth()->check() && isset($perms[0]->permission), 403, "You don't have the correct permission to be in this area.");
-        }
-
-        
+        }       
 
         return $next($request);
     }
